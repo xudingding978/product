025d2139c5050943497664952c4b803b2035e5a3
leosun <xsun051@hotmail.com>
2013-03-11 14:41:58 +1300

    'change model data to tpluserprofile'

--- app_useraccount/protected/modules/hybridauth/components/RemoteUserIdentity.php
+++ app_useraccount/protected/modules/hybridauth/components/RemoteUserIdentity.php
@@ -15,7 +15,7 @@ class RemoteUserIdentity extends CBaseUserIdentity {
 	 * @param Hybrid_Auth An instance of Hybrid_Auth 
 	 */
 	public function __construct($provider,Hybrid_Auth $hybridAuth) {
-		$this->loginProvider = $provider;
+		$this->LOGIN_PROVIDER = $provider;
 		$this->_hybridAuth = $hybridAuth;
 	}
 
@@ -24,7 +24,7 @@ class RemoteUserIdentity extends CBaseUserIdentity {
 	 * @return boolean whether authentication succeeds.
 	 */
 	public function authenticate() {
-		if (strtolower($this->loginProvider) == 'openid') {
+		if (strtolower($this->LOGIN_PROVIDER) == 'openid') {
 			if (!isset($_GET['openid-identity'])) {
 				throw new Exception('You chose OpenID but didn\'t provide an OpenID identifier');
 			} else {
@@ -34,12 +34,12 @@ class RemoteUserIdentity extends CBaseUserIdentity {
 			$params = array();
 		}
 		
-		$adapter = $this->_hybridAuth->authenticate($this->loginProvider,$params);
+		$adapter = $this->_hybridAuth->authenticate($this->LOGIN_PROVIDER,$params);
 		if ($adapter->isUserConnected()) {
 			$this->_adapter = $adapter;
-			$this->loginProviderIdentifier = $this->_adapter->getUserProfile()->identifier;
+			$this->LOGIN_PROVIDER_IDENTIFIER = $this->_adapter->getUserProfile()->identifier;
 
-			$user = HaLogin::getUser($this->loginProvider, $this->loginProviderIdentifier);
+			$user = TplUserProfile::getUser($this->LOGIN_PROVIDER, $this->LOGIN_PROVIDER_IDENTIFIER);
 			
 			if ($user == null) {
 				$this->errorCode = self::ERROR_USERNAME_INVALID;

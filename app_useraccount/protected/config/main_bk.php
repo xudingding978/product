0ad3b9a2d741a254e8177d8d65881af9af6e499d
leosun <xsun051@hotmail.com>
2013-03-13 10:50:04 +1300

    'edint main_bk'

--- app_useraccount/protected/config/main_bk.php
+++ app_useraccount/protected/config/main_bk.php
@@ -14,7 +14,7 @@
 
 return
         array(
-            "base_url" => "http://localhost/hybridauth-git/hybridauth/",
+            'baseUrl' => 'http://'.$_SERVER['HTTP_HOST'].'/hybridauth',
             "providers" => array(
                 "OpenID" => array(
                     "enabled" => true

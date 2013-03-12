c5c8f2819803b4ddf62f67aa8f7d11a1b8b1580a
leosun <xsun051@hotmail.com>
2013-03-13 10:37:07 +1300

    '// try to authenticate with twitter'

--- app_useraccount/protected/modules/hybridauth/views/default/index.php
+++ app_useraccount/protected/modules/hybridauth/views/default/index.php
@@ -12,33 +12,15 @@ $config = '/home/ubuntu/platform/hubstar/app_useraccount/protected/config/main.p
 echo dirname(__FILE__);
 require_once( '/home/ubuntu/platform/hubstar/app_useraccount/protected/modules/hybridauth/Hybrid/Auth.php' );
 
-try{
+
 $hybridauth = new Hybrid_Auth( $config );
  
-# try to authenticate with some providers
+
  
-$facebook = $hybridauth->authenticate( "Facebook" );
+ // try to authenticate with twitter
+$adapter = $hybridauth->authenticate( "Twitter" );
  
-$twitter = $hybridauth->authenticate( "Twitter" );
+// return Hybrid_User_Profile object intance
+$user_profile = $adapter->getUserProfile();
  
-# now try to play with theses social apis
-# Facebook: https://developers.facebook.com/docs/reference/api/
-// ask facebook for friends list
-$response = $facebook->api()->api('/me/friends');
-// Post to the user wall
-$response = $facebook->api()->api("/me/feed", "post", array(
-message => "Hi there",
-picture => "http://www.mywebsite.com/path/to/an/image.jpg",
-link => "http://www.mywebsite.com/path/to/a/page/",
-name => "My page name",
-caption => "And caption"
-));
-# Twitter: https://dev.twitter.com/docs/api
-// Returns the current count of friends, followers, updates (statuses) ...
-$response = $twitter->api()->get( 'account/totals.json' );
- 
-// You get the point
-}
-catch( Exception $e ){
-echo "Ooophs, we got an error: " . $e->getMessage();
-}
+echo "Hi there! " . $user_profile->displayName;

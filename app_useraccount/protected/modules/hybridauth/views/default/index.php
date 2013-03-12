3bb173fd32160748d64a853bae177a1bcf0f6ef3
leosun <xsun051@hotmail.com>
2013-03-13 10:06:57 +1300

    ''

--- app_useraccount/protected/modules/hybridauth/views/default/index.php
+++ app_useraccount/protected/modules/hybridauth/views/default/index.php
@@ -8,9 +8,9 @@
 //session_start();
 // change the following paths if necessary
 //$config = dirname(__FILE__) . '/library/config.php';
-$config = '../../../../config/main.php';
+$config = 'main.php';
 echo dirname(__FILE__);
-require_once( $config);
+include_once( 'Auth.php' );
 try {
     // create an instance for Hybridauth with the configuration file path as parameter
     $hybridauth = new Hybrid_Auth($config);

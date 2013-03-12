201da4875ca181551f5665cdd87e2aa1431b47d7
leosun <xsun051@hotmail.com>
2013-03-13 10:57:36 +1300

    '$config = '/home/ubuntu/platform/hubstar/app_useraccount/protected/config/main_bk.php';'

--- app_useraccount/protected/modules/hybridauth/views/default/index.php
+++ app_useraccount/protected/modules/hybridauth/views/default/index.php
@@ -6,9 +6,9 @@
 
 // start a new session (required for Hybridauth)
 //session_start();
-// change the following paths if necessary
-//$config = dirname(__FILE__) . '/library/config.php';
-$config = '/home/ubuntu/platform/hubstar/app_useraccount/protected/config/main_bk.php';
+ 
+//$config = '/home/ubuntu/platform/hubstar/app_useraccount/protected/config/main_bk.php';
+$config = '/home/ubuntu/platform/hubstar/app_useraccount/protected/config/main.php';
 echo dirname(__FILE__);
 require_once( '/home/ubuntu/platform/hubstar/app_useraccount/protected/modules/hybridauth/Hybrid/Auth.php' );
 

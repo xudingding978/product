697f25a1b9e4d2af700f662dd6066d4941eb66b2
leosun <xsun051@hotmail.com>
2013-03-14 15:12:44 +1300

    'config fille address'

--- app_useraccount/protected/modules/hybridauth/controllers/DefaultController.php
+++ app_useraccount/protected/modules/hybridauth/controllers/DefaultController.php
@@ -113,11 +113,11 @@ class DefaultController extends CController {
     }
 
     private function _linkProvider($identity) {
-        $config = Yii::app()->getBasePath() . '/config/provider_config.php';
-        require_once( Yii::app()->getBasePath() . '/modules/hybridauth/Hybrid/Auth.php');
+//        $config = Yii::app()->getBasePath() . '/config/provider_config.php';
+ //       require_once( Yii::app()->getBasePath() . '/modules/hybridauth/Hybrid/Auth.php');
         
-//        $config = '/home/devbox/NetBeansProjects/bds-v3.1/app_useraccount/protected/config/provider_config.php';
-//        require_once( '/home/devbox/NetBeansProjects/bds-v3.1/app_useraccount/protected/modules/hybridauth/Hybrid/Auth.php' );
+       $config = '/home/devbox/NetBeansProjects/bds-v3.1/app_useraccount/protected/config/provider_config.php';
+        require_once( '/home/devbox/NetBeansProjects/bds-v3.1/app_useraccount/protected/modules/hybridauth/Hybrid/Auth.php' );
         
         $hybridauth = new Hybrid_Auth($config);
         $adapter = $hybridauth->authenticate($_GET['provider']);

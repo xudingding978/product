62499e28ba3fe18ea76965098d5a7a249a63ed1a
leosun <xsun051@hotmail.com>
2013-03-14 15:02:19 +1300

    'fix the REC_ID problem'

--- app_useraccount/protected/views/user/_view.php
+++ app_useraccount/protected/views/user/_view.php
@@ -6,11 +6,11 @@
 <div class="view">
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('REC_ID')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'REC_ID'=>$data->id)); ?>
+	<?php echo CHtml::link(CHtml::encode($data->REC_ID), array('view', 'REC_ID'=>$data->REC_ID)); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('USER_NAME')); ?>:</b>
-	<?php echo CHtml::encode($data->username); ?>
+	<?php echo CHtml::encode($data->USER_NAME); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('PWD_HASH')); ?>:</b>

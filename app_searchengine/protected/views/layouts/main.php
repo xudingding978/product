ff92ede9af90cf70b465c5cfea4aec22703fcd35
leosun <xsun051@hotmail.com>
2013-04-05 16:06:39 +1300

    get facbook buttom

--- app_searchengine/protected/views/layouts/main.php
+++ app_searchengine/protected/views/layouts/main.php
@@ -15,7 +15,7 @@
 
             <link media="screen, projection" rel="stylesheet" href="../../../css/reset-new.css"/>
 
-
+            <script type="text/javascript" language="JavaScript" src="../../../js/ember-1.0.0-rc.2.js"></script>  
 
             <link rel="stylesheet" type="text/css" href="../../../css/main.css" />    
 
@@ -25,6 +25,7 @@
             <title><?php echo 'search engine' ?></title>
             <script type="text/javascript" language="JavaScript" src="../../../js/jquery-1.7.1.min.js"></script>
             <script type="text/javascript" language="JavaScript" src="../../../js/jquery.isotope.min.js"></script>  
+
 
             <link media="screen, projection" rel="stylesheet" type="text/css" href="../../../css/gallery.css"/>
 

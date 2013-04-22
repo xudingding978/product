391428f2466ac03c1e07e40c0884b149c3e771c3
devbox <devbox@devbox4.(none)>
2013-04-22 16:06:57 +1200

    create a new profile page

---
+++ app_searchengine/protected/views/profile/index.php
@@ -0,0 +1,18 @@
+<?php
+/* @var $this ProfileController */
+/* @var $form CActiveForm  */
+
+$this->pageTitle = Yii::app()->name . ' - Profile';
+$this->breadcrumbs = array(
+    'Profile',
+);
+?>
+<div style="margin-top:25px;"></div>
+<h1>
+    This is the profile page!
+    
+</h1>
+     <h2>
+         Colin, please help me! I need designing!
+         
+    </h2>

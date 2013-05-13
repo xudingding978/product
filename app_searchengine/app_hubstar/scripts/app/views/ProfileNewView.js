628bf8d91a3129e7c5598f60dcafb47fb9d79088
leosun <xsun051@hotmail.com>
2013-05-13 12:10:49 +1200

    fixed profile template conflict

--- app_searchengine/app_hubstar/scripts/app/views/ProfileNewView.js
+++ app_searchengine/app_hubstar/scripts/app/views/ProfileNewView.js
@@ -1,22 +1,22 @@
 define([
     "ember",
-    "text!templates/profileNewTemplate.html", 
-    'models/ProfileModel'
-   
-
-], function(Ember, profileNewTemplate, ProfileModel) {
+    "text!templates/profileNewTemplate.html",
+    'models/ProfileModel',
+    'controllers/ProfileController',
+], function(Ember, profileNewTemplate, ProfileModel, ProfileController) {
     Ember.TEMPLATES["profileNew"] = Ember.Handlebars.compile(profileNewTemplate);
 
 
     var ProfileNew = Ember.View.extend({
         template: Ember.Handlebars.compile(profileNewTemplate),
+        controller: ProfileController,
         save: function(router) {
             var new_name = this.$("#profile_name").val();
             var new_id = this.$("#id").val();
-            alert(new_name);
-            console.log(ProfileModel.find("leo"));
+            ProfileController.newProfile();
+
             var newProfile = ProfileModel.createRecord({"id": new_id, "profile_name": new_name});
-            
+
 //           route.transitionTo('profile', newProfile);
 
         }

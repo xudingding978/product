d76901b52de8b19b5898fd7c477e4122c3237208
leosun <xsun051@hotmail.com>
2013-05-16 11:50:43 +1200

    roll back

--- app_searchengine/app_hubstar/scripts/app/controllers/ProfileController.js
+++ app_searchengine/app_hubstar/scripts/app/controllers/ProfileController.js
@@ -5,34 +5,41 @@ define([
                 ProfileModel,
                 Ember
                 ) {
-            var record;
+            var profile_record;
+            var about_record;
             var ProfileController = Ember.ObjectController.extend({
                 editing: false,
                 content: null,
 //                record: Ember.Profile.find(this.get('content.profile_name')),
                 toggleEditing: function() {
-                    record = this.get('content.profile_name');
+                    profile_record = this.get('content.profile_name');
                     this.set('editing', !this.get('editing'));
 
                 },
                 changeTitle: function() {
 
-                    var update_record = App.Profile.find(this.get('content.id'));
-                    App.store.get('adapter').updateRecord(App.store, App.Profile, update_record);
+                    var update_profil_record = App.Profile.find(this.get('content.id'));
+                    App.store.get('adapter').updateRecord(App.store, App.Profile, update_profil_record);
                     App.store.commit();
                     this.set('editing', false);
                 },
                 exitEditing: function() {
-                    this.set('content.profile_name', record);
+                    this.set('content.profile_name', profile_record);
                     this.set('editing', !this.get('editing'));
                 },
                 toggleEditingAbout: function() {
+                    about_record = this.get('content.about');
                     this.set('editingAbout', !this.get('editingAbout'));
-
                 },
                 changeAbout: function() {
-
+                    var update_about_record = App.Profile.find(this.get('content.id'));
+                    App.store.get('adapter').updateRecord(App.store, App.Profile, update_about_record);
+                    App.store.commit();
                     this.set('editingAbout', false);
+                },
+                exitAboutEditing: function() {
+                    this.set('content.about', about_record);
+                    this.set('editingAbout', !this.get('editingAbout'));
                 },
                 toggleEditingContact: function() {
 

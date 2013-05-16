dd5a2df5a20e95c3730f218be65b74d68aada610
leosun <xsun051@hotmail.com>
2013-05-16 16:27:09 +1200

    wysihtml5 fake version

--- app_searchengine/protected/views/layouts/frontEnd.php
+++ app_searchengine/protected/views/layouts/frontEnd.php
@@ -14,7 +14,7 @@
         <link rel="stylesheet" href="css/reset-new.css">
 
         <link rel="stylesheet" href="/app_hubstar/scripts/libs/bootstrap/2.2.2/css/bootstrap.css">
-
+        <link rel="stylesheet" href="/app_hubstar/css/bootstrap-wysihtml5.css">
         <link rel="stylesheet" href="/app_hubstar/css/style.css">
         <link rel="stylesheet" type="text/css" href="css/customstyle.php" /> 
         <link media="screen, projection" rel="stylesheet" type="text/css" href="css/_topnavbar.css"/>

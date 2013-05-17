1a88c78c5002b7464a79fe7a7c8c34d8659dd660
devbox <devbox@devbox4.(none)>
2013-05-17 15:24:02 +1200

    templatenew done

--- app_searchengine/protected/views/layouts/frontEnd.php
+++ app_searchengine/protected/views/layouts/frontEnd.php
@@ -20,6 +20,7 @@
         <link media="screen, projection" rel="stylesheet" type="text/css" href="css/_topnavbar.css"/>
         <link media="screen, projection" rel="stylesheet" type="text/css" href="css/_footer.css"/>
         <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
         <script src="app_hubstar/scripts/libs/bootstrap/2.2.2/js/bootstrap.min.js"></script>
         <script src="app_hubstar/scripts/libs/jquery.bxslider.min.js"></script>
         <link rel="stylesheet" href="/app_hubstar/css/jquery.bxslider.css">

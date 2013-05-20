0021a2c2238d9c916a600ce70c31077f5983242d
leosun <xsun051@hotmail.com>
2013-05-21 09:49:05 +1200

    searchcontroller

--- app_searchengine/app_hubstar/scripts/app/helpers/forms.js
+++ app_searchengine/app_hubstar/scripts/app/helpers/forms.js
@@ -1,23 +1,30 @@
 
 // Start the main app logic.
 requirejs(['handlebars'],
-function   () {
+        function() {
 
-    Handlebars.registerHelper('submitButton', function(text) {
-      return new Handlebars.SafeString('<button  id="login" type="submit" class="btn btn-primary">' + text + '</button>');
-    });
+            Handlebars.registerHelper('submitButton', function(text) {
+                return new Handlebars.SafeString('<button  id="login" type="submit" class="btn btn-primary">' + text + '</button>');
+            });
 
-    Handlebars.registerHelper('plainButton', function(text) {
-      return new Handlebars.SafeString('<button   type="submit" class="btn btn-primary">' + text + '</button>');
-    });
+            Handlebars.registerHelper('plainButton', function(text) {
+                return new Handlebars.SafeString('<button   type="submit" class="btn btn-primary">' + text + '</button>');
+            });
 
-    Handlebars.registerHelper('mailto', function(field) {
-      var address = this.get(field);
-      if (address) {
-        return new Handlebars.SafeString('<a href="mailto: ' + address + '" />' + address + '</a>');
-      }
-    });
+            Handlebars.registerHelper('mailto', function(field) {
+                var address = this.get(field);
+                if (address) {
+                    return new Handlebars.SafeString('<a href="mailto: ' + address + '" />' + address + '</a>');
+                }
+            });
 
 
-});
+            Handlebars.registerHelper('checkingType', function(object) {
+                return new Handlebars.SafeString(
+                        "<a href='" + object.url + "'>" + object.text + "</a>"
+                        );
+            });
+
+
+        });
 

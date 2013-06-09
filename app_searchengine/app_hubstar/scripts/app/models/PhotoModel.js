812afaa1836baac6a53f5cc81192f5bd75379bbc
leosun <xsun051@hotmail.com>
2013-06-10 08:01:27 +1200

    loaclstore

--- app_searchengine/app_hubstar/scripts/app/models/PhotoModel.js
+++ app_searchengine/app_hubstar/scripts/app/models/PhotoModel.js
@@ -11,7 +11,6 @@ define(
         mega: DS.hasMany('App.Object'),
         photo_title: DS.attr('string'),
         photo_image_url: DS.attr('string'),
-        image_url: DS.attr('string'),
         photo_image_linkto: DS.attr('string'),
         photo_caption: DS.attr('string'),
         photo_type: DS.attr('string'), //
@@ -21,13 +20,13 @@ define(
         photo_keywords: DS.attr('string'), //[ ]
         photo_brands: DS.attr('string'), // [ ]
         photo_products: DS.attr('string'), // [ ] 
-
+        photo_articleId: DS.attr('string'),
+        photo_heliumId: DS.attr('string'),
         didLoad: function() {
 //            console.log('model loaded', this.toJSON());
 //            console.log('id: ' + this.id + ' ' + this.profile_name, this);
         }
     });
-
     return Photo;
 }
 );

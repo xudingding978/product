f222465f2551dd0e179ad01aaf43c956b43a50cc
leosun <xsun051@hotmail.com>
2013-06-11 10:13:00 +1200

    photo template

--- app_searchengine/app_hubstar/scripts/app/views/SearchsView.js
+++ app_searchengine/app_hubstar/scripts/app/views/SearchsView.js
@@ -4,7 +4,24 @@ define([
 ], function(Ember, searchTemplate) {
     Ember.TEMPLATES["searchs"] = Ember.Handlebars.compile(searchTemplate);
     var searchView = Ember.View.extend({
-        template: Ember.Handlebars.compile(searchTemplate)
+        template: Ember.Handlebars.compile(searchTemplate),
+        didInsertElement: function() {
+
+
+            if (App.get('isLogin')) {
+
+                $('#login_icon').attr("style", "display:none");
+                $('#login_detail').attr("style", "display:block");
+
+
+
+            } else {
+
+                $('#login_icon').attr("style", "display:block");
+                $('#login_detail').attr("style", "display:none");
+
+            }
+        }
 
     });
 

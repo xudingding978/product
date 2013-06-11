99f7e6710e55b475abd396784f8d3b3dc6815ec3
leosun <xsun051@hotmail.com>
2013-06-11 13:32:27 +1200

    user template

--- app_searchengine/app_hubstar/scripts/app/router.js
+++ app_searchengine/app_hubstar/scripts/app/router.js
@@ -28,15 +28,21 @@ define(["ember"], function(Ember) {
                 this.resource("profile", {path: ':profile_id'
                 });
             });
+
+            this.resource("users", {path: '/users/:user_id'});
+            this.resource("users", function() {
+                //           this.resource("profileIndex", {path: '/'});
+
+                this.resource("user", {path: ':user_id'
+                });
+            });
             this.resource("searchs", {path: "/search"}, function( ) {
                 this.resource("searchIndex", {path: '/'});
                 this.resource('search', {path: ':search_id'});
             });
         });
 
-        this.resource("users", {
-            path: "/users"
-        });
+
         this.resource("test", {
             path: "/test"
         });

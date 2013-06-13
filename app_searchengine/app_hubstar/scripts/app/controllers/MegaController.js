36d3037bdfdc3e74b36cd82b53bf9ec48e1d80ce
leosun <xsun051@hotmail.com>
2013-06-13 19:19:15 +1200

    view collection

--- app_searchengine/app_hubstar/scripts/app/controllers/MegaController.js
+++ app_searchengine/app_hubstar/scripts/app/controllers/MegaController.js
@@ -10,6 +10,7 @@ define(['models/MegaModel',
                 MegaModel,
                 Ember
                 ) {
+            var tcontent;
             var MegaController = Ember.ArrayController.extend({
                 content: [],
                 percentComplete: 0,
@@ -26,11 +27,12 @@ define(['models/MegaModel',
                     }
                     return 0;
                 },
-                previesImage: function() {
+                previesImage: function(e) {
+                    alert(e.id);
                     this.addObjects();
-//                    if (!this.get('selected')) {
-//                        this.set('selected', this.get('content').get('lastObject'));
-//                    }
+                    if (!this.get('selected')) {
+                        this.set('selected', this.get('content').get('lastObject'));
+                    }
                     var selectedIndex = this.findSelectedItemIndex();
 
                     if (selectedIndex <= 0) {
@@ -40,63 +42,76 @@ define(['models/MegaModel',
                     }
 
                     this.set('selected', this.get('content').objectAt(selectedIndex));
-                    //         console.log(this.get('selected'));
 
-                    this.set("percentComplete", this.get('content').objectAt(selectedIndex));
 
+                    this.set("percentComplete", this.get('selected'));
+                    console.log(this.get('percentComplete'));
 
                 },
-                nextImage: function() {
-
+                nextImage: function(e) {
+                    alert(e);
                     this.addObjects();
 
-//                    if (!this.get('selected')) {
-//
-//                        this.set('selected', this.get('content').get('firstObject'));
-//
-//                    }
+                    if (!this.get('selected')) {
+
+                        this.set('selected', this.get('content').get('firstObject'));
+
+                    }
 
                     var selectedIndex = this.findSelectedItemIndex();
                     if (selectedIndex >= (this.get('content').get('length') - 1)) {
 
                         selectedIndex = 0;
                     } else {
-
                         selectedIndex++;
 
                     }
                     this.set('selected', this.get('content').objectAt(selectedIndex));
-                    console.log(this.get('content').objectAt(selectedIndex).record._data);
-                    //      this.set("percentComplete", this.get('content').objectAt(selectedIndex).data.photo[0]);
-                    //    console.log(this.get('content'));
-                    //   console.log(this.get('content').objectAt(selectedIndex).editors);
-                    //        console.log(this.get('selected'));
-
+                    console.log(this.get('selected'));
+                    if (this.get('selected').data.materialized) {
+                        this.set("percentComplete", this.get('selected').prematerialized.photo[0].data);
+                        console.log("materialized");
+                        console.log(this.get('percentComplete'));
+                    } else {
+                        this.set("percentComplete", this.get('selected').data.photo[0]);
+                        console.log("nottttttttttt   materialized");
+                        console.log(this.get('percentComplete'));
+                    }
                 },
                 actionOn: function(megaObject) {
+                    //  console.log("aaaaa" + megaObject);
                     var content = this.get("content");
-                    console.log(megaObject);
-                    console.log("owner_profile_id: " + megaObject.get("owner_profile_id"));
-                    console.log("collection_id: " + megaObject.get("collection_id"));
-                    //                 console.log(megaObject);
-                    //      content.pushObject(megaObject);
-                 
-               //     setTimeout(function() {
+
+                    //     var t = MegaModel.find(megaObject);
+                    //    console.log(megaObject);
+                    //     content.pushObject(megaObject);
+                    setTimeout(function() {
                         var owner_profile_id = megaObject.get("owner_profile_id");
                         var collection_id = megaObject.get("collection_id");
                         tcontent = MegaModel.find({"collection_id": collection_id, "owner_profile_id": owner_profile_id});
-                        console.log(tcontent);
-                        this.addObjects(tcontent);
 
+
+
+                        //      console.log("aaaaaaaaa   "+tcontent.get("firstObject"));
+//                        tcontent.one("didLoad", function() {
+//                            tcontent.resolve(tcontent.get("firstObject"));
+//                        });
+                    }, 200);
                 },
-                addObjects: function(tcontent) {
-console.log("aaaaa");
-console.log(tcontent.self);
+                imgReturn: function() {
+                }.observes('selected'),
+                addObjects: function() {
+
+                    if (!this.isSelected)
+                    {
+
                         for (var i = 0; i < tcontent.get("content").get("length"); i++) {
-console.log(i);
+                            //    this.content.pushObject(tcontent);
                             this.content.pushObject(tcontent.get("content").objectAt(i));
                         }
-                        this.isSelected = true;                    
+                        this.isSelected = true;
+                    }
+
                 }
 
 

444854032e64ed391c6a17507d9af0495fd47e4c
Kingsley Huang <huangkingsley@gmail.com>
2013-06-14 14:07:40 +1200

    collection version 3

--- app_searchengine/app_hubstar/scripts/app/controllers/SearchsController.js
+++ app_searchengine/app_hubstar/scripts/app/controllers/SearchsController.js
@@ -3,14 +3,29 @@ define([
     'ember'
 ], function(MegaModel, Ember) {
     var SearchsController = Ember.ArrayController.extend({
+        content: [],
         newSearch: function(object) {
-            
-            
-            
-      
-            
-         this.set("controller",[]);
-            App.Mega.find(object);            
+            //      
+            console.log("serach");
+
+//    var data = MegaModel.find(object);
+//                    data.addObserver('isLoaded', function() {
+//                        if (data.get('isLoaded')) {
+//                            for (var i = 0; i < this.get("content").get("length"); i++) {
+//                          //      midcontent.pushObject((this.get("content").objectAt(i)));
+//                                    console.log(this.get("content").objectAt(i));
+//                            }
+//                        }
+//
+//                    });
+
+
+                this.set("content", MegaModel.find(object));
+                
+                
+                
+     //       this.get("content").pushObject(MegaModel.find(object));            
+        console.log(this.get("content").get("length"));
 //            var searchResult = App.store.createRecord(App.Search, {
 //                id: object.id,
 //                region: object.region,
@@ -18,7 +33,7 @@ define([
 //            });
 
 
-     //       this.transitionToRoute('search', searchResult);
+            //       this.transitionToRoute('search', searchResult);
 
         }
 

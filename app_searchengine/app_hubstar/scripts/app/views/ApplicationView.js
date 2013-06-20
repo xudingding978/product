7ad2a36cdc8b2acc65d2a7bf40b4ccb783473258
Jason Liddiard <jason@the-ebusiness-company.com>
2013-06-21 08:13:07 +1200

    trying to accessing app

--- app_searchengine/app_hubstar/scripts/app/views/ApplicationView.js
+++ app_searchengine/app_hubstar/scripts/app/views/ApplicationView.js
@@ -1,15 +1,14 @@
 define([
+    "App",
     "ember",
     "text!templates/applicationTemplate.html"
-], function(Ember, applicationTemplate) {
+], function(App, Ember, applicationTemplate) {
 
     Ember.TEMPLATES["application"] = Ember.Handlebars.compile(applicationTemplate);
 
     var ApplicationView = Ember.View.extend({
         defaultTemplate: Ember.Handlebars.compile(applicationTemplate),
-
-
-
     });
+
     return ApplicationView;
 });

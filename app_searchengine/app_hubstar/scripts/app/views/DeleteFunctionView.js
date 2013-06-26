df57aaa0e9884a7b4764d16ceb54d948adffadc0
leosun <xsun051@hotmail.com>
2013-06-26 17:02:41 +1200

    big change

--- app_searchengine/app_hubstar/scripts/app/views/DeleteFunctionView.js
+++ app_searchengine/app_hubstar/scripts/app/views/DeleteFunctionView.js
@@ -9,7 +9,7 @@ define([
         classNames: ['blur'],
         template: Ember.Handlebars.compile(deletefunctionTemplate),
         didInsertElement: function() {
-            this.$().hide().show('slow');
+         //   this.$().hide().show('fast');
         },
         deleteSelection: function() {
             var controller = this.get('controller');
@@ -36,6 +36,34 @@ define([
             else {
                 console.log(controller);
             }
+        },
+        cancelDelete: function() {
+            var controller = this.get('controller');
+            if (controller._debugContainerKey.indexOf("addCollection") !== -1)
+            {
+                //     controller.addNewCollection();
+            }
+            else if (controller._debugContainerKey.indexOf("application") !== -1)
+            {
+
+                //       controller.newSearch();
+            }
+            else if (controller._debugContainerKey.indexOf("mega") !== -1) {
+
+
+                //    controller.addComment();
+
+            } else if (controller._debugContainerKey.indexOf("user") !== -1) {
+
+
+                controller.cancelDelete();
+
+            }
+            else {
+                console.log(controller);
+            }
+
+
         }
 
 

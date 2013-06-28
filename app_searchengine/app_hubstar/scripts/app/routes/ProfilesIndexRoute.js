475dd4581e1addf780809a605efa11527f2e7c68
leosun <xsun051@hotmail.com>
2013-06-29 10:20:32 +1200

    big change

--- app_searchengine/app_hubstar/scripts/app/routes/ProfilesIndexRoute.js
+++ app_searchengine/app_hubstar/scripts/app/routes/ProfilesIndexRoute.js
@@ -17,6 +17,7 @@ define([
             if ((localStorage.getItem("loginStatus") === null) || (localStorage.loginStatus === "")) {
 
                 this.transitionTo('indexIndex');
+                  this.controllerFor('application').set('popup', true);
 
             } else {
 

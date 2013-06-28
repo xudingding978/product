764b1d58fa2f2094be33504c69b9eff70a3841c9
leosun <xsun051@hotmail.com>
2013-06-28 19:12:15 +1200

    big change

--- app_searchengine/app_hubstar/scripts/app/views/CommentView.js
+++ app_searchengine/app_hubstar/scripts/app/views/CommentView.js
@@ -36,12 +36,18 @@ define([
             $('#closeComment_' + id).attr('style', 'display:block');
             $('#showMoreComment_' + id).attr('style', 'display:none');
             $('#commentData_' + id).attr('style', 'max-height:100%');
+            setTimeout(function() {
+                $('#masonry_container').masonry("reload");
+            }, 200);
 
         },
         closeMore: function(id) {
             $('#closeComment_' + id).attr('style', 'display:none');
             $('#showMoreComment_' + id).attr('style', 'display:block');
-            $('#commentData_' + id).attr('style', 'max-height:110px');
+            $('#commentData_' + id).attr('style', 'max-height:120px');
+            setTimeout(function() {
+                $('#masonry_container').masonry("reload");
+            }, 200);
         }
     });
 

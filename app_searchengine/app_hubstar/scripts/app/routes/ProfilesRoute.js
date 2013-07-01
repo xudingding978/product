7559663b3b1a30dd4b43a795c693c610773c39f8
leosun <xsun051@hotmail.com>
2013-07-01 14:46:42 +1200

    big change

--- app_searchengine/app_hubstar/scripts/app/routes/ProfilesRoute.js
+++ app_searchengine/app_hubstar/scripts/app/routes/ProfilesRoute.js
@@ -10,16 +10,16 @@ define([
     "use strict";
     var ProfilesRoute = Ember.Route.extend({
         //     controller: ApplicationController,
-//        setupController: function(controller, ProfileModel) {
-//            this.controllerFor('application').set('islogin', true);
-//            this.controllerFor('application').set('popup', false);
-//            this.controllerFor('application').set('isotherpage', true);
-//            this.controllerFor('searchs').setLoginImge();
-//            this.controllerFor('application').set('isotherpage', true);
-//        },
-//        activate: function() {
-//
-//        },
+        setupController: function(controller, ProfileModel) {
+            this.controllerFor('application').set('islogin', true);
+            this.controllerFor('application').set('popup', false);
+            this.controllerFor('application').set('isotherpage', true);
+            this.controllerFor('searchs').setLoginImge();
+            this.controllerFor('application').set('isotherpage', true);
+        },
+        activate: function() {
+
+        },
 //        model: function() {
 //            return ProfileModel.find();
 //        },

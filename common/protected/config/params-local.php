3def2fd2470d963806533352784211cff13733e9
Jason Liddiard <jason@the-ebusiness-company.com>
2013-07-03 10:11:36 +1200

    Updated the Profile new with spelling corrections

--- common/protected/config/params-local.php
+++ common/protected/config/params-local.php
@@ -28,11 +28,11 @@ return array(
     
     //ElasticSearch Configurations
     'elasticSearch.node' => 'es1.hubsrv.com',
-    'elasticSearch.index' => 'test',
+    'elasticSearch.index' => 'develop',
     
     //CouchBase Configurations
     'couchBase.node'=>'cb1.hubsrv.com:8091',
-    'couchBase.bucket'=>'test',
+    'couchBase.bucket'=>'develop',
     
       //MySQL Database Configurations
     'db_live.name' => 'db_live',

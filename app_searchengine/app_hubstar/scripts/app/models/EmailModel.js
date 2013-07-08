8a4b7c44d1b41574d88620c8c0d6f044411f0bcb
leosun <xsun051@hotmail.com>
2013-07-09 09:03:44 +1200

    big change

--- app_searchengine/app_hubstar/scripts/app/models/EmailModel.js
+++ app_searchengine/app_hubstar/scripts/app/models/EmailModel.js
@@ -1,25 +1,29 @@
 define(
-        'models/CategoryModel',
+        'models/EmailModel',
         [
             'ember',
             'emberData'
         ], function() {
 
-    var CategoryModel = DS.Model.extend({
-
-
-
-
-
-
-
+    var EmailModel = DS.Model.extend({
+        displayName: DS.attr('string'),
+        displayEmail: DS.attr('string'),
+        emailBody: DS.attr('string'),
+        emailSubject: DS.attr('string'),
+        emailDestination: DS.attr('string'),
+        emaiCCDestination: DS.attr('string'),
+        emaiBCCDestination: DS.attr('string'),
+        projectCategory: DS.attr('string'),
+        projectTimeframe: DS.attr('string'),
+        projectBudget: DS.attr('string'),
+        projectExperience: DS.attr('string'),
         didLoad: function() {
 //            console.log('model loaded', this.toJSON());
 //            console.log('id: ' + this.id + ' ' + this.profile_name, this);
         }
     });
 
-    return CategoryModel;
+    return EmailModel;
 }
 );
 

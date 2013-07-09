9eadff1253b68d420ae8fac463a0218932bdadde
leosun <xsun051@hotmail.com>
2013-07-10 10:21:45 +1200

    big change

--- app_searchengine/app_hubstar/scripts/app/controllers/PlatformBarController.js
+++ app_searchengine/app_hubstar/scripts/app/controllers/PlatformBarController.js
@@ -7,15 +7,15 @@ define(["ember", 'models/CateModel'], function(Ember, CateModel) {
         init: function()
         {
 
-            this.set("user", App.User.find(localStorage.loginStatus));
-            console.log(this.get('user'));
-            this.set("myUserProfile", "#/users/" + localStorage.loginStatus);
+
 
         },
         toMyCollection: function() {
 
         },
         setTopicModel: function(model) {
+            this.set("user", App.User.find(localStorage.loginStatus));
+            this.set("myUserProfile", "#/users/" + localStorage.loginStatus);
             this.set('categorys', null);
             this.set('categorys', model);
             var category = this.get('categorys');

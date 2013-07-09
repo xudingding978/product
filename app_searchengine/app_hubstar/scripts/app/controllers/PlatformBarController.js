a7dae0379a211fbfbbcd5e1b91f1ecc94e798165
leosun <xsun051@hotmail.com>
2013-07-10 11:27:58 +1200

    big change

--- app_searchengine/app_hubstar/scripts/app/controllers/PlatformBarController.js
+++ app_searchengine/app_hubstar/scripts/app/controllers/PlatformBarController.js
@@ -20,27 +20,10 @@ define(["ember"], function(Ember) {
 
         },
         setTopicModel: function(model) {
-            console.log(model.get('content'));
             this.set("user", App.User.find(localStorage.loginStatus));
             this.set("myUserProfile", "#/users/" + localStorage.loginStatus);
             this.set('categorys', null);
             this.set('categorys', model);
-//            var category = this.get('categorys');
-//            var that = this;
-//            setTimeout(function() {
-//                for (var i = 0; i < category.get("length"); i++) {
-//
-//                    for (var j = 0; j < category.objectAt(i).get('subcate').get("length"); j++) {
-//                        var data = category.objectAt(i).get('subcate').objectAt(j).get('subcategories');
-//                        //      console.log(data);
-//
-//                        that.get('centent').pushObject(data);
-//
-//                    }
-//
-//                }
-//
-//            }, 1);
 
         }
     });

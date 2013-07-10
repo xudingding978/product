714989ff5cc8a02ad182db502d9d3e70ba16cb26
leosun <xsun051@hotmail.com>
2013-07-10 13:23:36 +1200

    big change

--- app_searchengine/app_hubstar/scripts/app/controllers/ItemProfilesController.js
+++ app_searchengine/app_hubstar/scripts/app/controllers/ItemProfilesController.js
@@ -1,8 +1,10 @@
 define(["ember"], function(Ember) {
     var ItemProfilesController = Ember.Controller.extend({
-        
-        
-        
+        profiles: null,
+        init: function() {
+            this.set("profiles", App.Mega.find());
+            console.log();
+        },
     });
     return ItemProfilesController;
 });

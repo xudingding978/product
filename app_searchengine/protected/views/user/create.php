f436fefcc80c564f515561225665fd04a245131d
leosun <xsun051@hotmail.com>
2013-07-14 11:03:46 +1200

    user colleciton addding

---
+++ app_searchengine/protected/views/user/create.php
@@ -0,0 +1,11 @@
+<?php
+/* @var $this UserController */
+/* @var $model User */
+error_log('ssssssssssssssssssssssssssssssss');
+
+
+?>
+
+<h1>Create User</h1>
+
+<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>

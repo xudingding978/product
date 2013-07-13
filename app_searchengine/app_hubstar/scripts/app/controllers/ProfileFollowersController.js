de3cc36c8e04a7fc43b585e6e473213f9978e244
leosun <xsun051@hotmail.com>
2013-07-13 14:32:10 +1200

    big change

--- app_searchengine/app_hubstar/scripts/app/controllers/ProfileFollowersController.js
+++ app_searchengine/app_hubstar/scripts/app/controllers/ProfileFollowersController.js
@@ -1,16 +1,15 @@
 define(["ember"], function(Ember) {
     var ProfileFollowersController = Ember.Controller.extend({
         content: [],
-        followers:"",
-        
+        followers: "",
         getClientId: function(model) {
             console.log(model);
             this.set('clientID', model.id);
             this.set('followers', model.get('followers'));
             console.log(this.get('clientID'));
             console.log(this.get('followers'));
-         //   var data = App.Mega.find({RequireType: "partner", profile_partner_ids: this.get('partnerID')});
-       this.set('content', this.get('followers'));
+            //   var data = App.Mega.find({RequireType: "partner", profile_partner_ids: this.get('partnerID')});
+            this.set('content', this.get('followers'));
 
 
 

1dbb51ffaa51f536372d6784450e0ddd8cb9a8a5
Kingsley Huang <huangkingsley@gmail.com>
2013-07-22 14:58:25 +1200

    add partner function fixed

--- app_searchengine/app_hubstar/scripts/app/controllers/MasonryCollectionItemsController.js
+++ app_searchengine/app_hubstar/scripts/app/controllers/MasonryCollectionItemsController.js
@@ -167,6 +167,9 @@ define([
                     }
                 }
             });
+        },
+        checkEditingMode: function()
+        {
         }
     });
     return MasonryCollectionItemsController;

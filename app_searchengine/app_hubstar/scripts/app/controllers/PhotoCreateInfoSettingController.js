98f296412ed63338d86fa8c41c8f30ce8dced2fc
leosun <xsun051@hotmail.com>
2013-07-22 14:05:33 +1200

    profile url invalide cahr

--- app_searchengine/app_hubstar/scripts/app/controllers/PhotoCreateInfoSettingController.js
+++ app_searchengine/app_hubstar/scripts/app/controllers/PhotoCreateInfoSettingController.js
@@ -1,7 +1,13 @@
 define(["ember"], function(Ember) {
     var PhotoCreateInfoSettingController = Ember.Controller.extend({
-      
-    }
-    );
+        needs: ['masonryCollectionItems'],
+        backToDragAndDrop: function() {
+
+            var masonryCollectionItems = this.get('controllers.masonryCollectionItems');
+            masonryCollectionItems.set('uploadOrsubmit', !masonryCollectionItems.get('uploadOrsubmit'));
+        }
+
+
+    });
     return PhotoCreateInfoSettingController;
 });

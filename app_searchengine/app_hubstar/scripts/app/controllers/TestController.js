5ce522f6082f2dad1ac4f159e694f6b42ced2232
Kingsley Huang <huangkingsley@gmail.com>
2013-07-31 14:06:17 +1200

    single photo uploader

--- app_searchengine/app_hubstar/scripts/app/controllers/TestController.js
+++ app_searchengine/app_hubstar/scripts/app/controllers/TestController.js
@@ -1,6 +1,8 @@
 define(["ember"], function(Ember) {
     var TestController = Ember.Controller.extend({
-      
+        test: function()
+        {console.log("test");
+        }
     }
     );
     return TestController;

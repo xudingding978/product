233b642ba24eb51715b755ee5d3362ff79446af9
Leo Sun <xsun051@hotmail.com>
2013-08-12 13:31:07 +1200

    dsad

--- app_restAPI/protected/components/Controller.php
+++ app_restAPI/protected/components/Controller.php
@@ -140,10 +140,6 @@ class Controller extends CController {
     protected function getRequestResult($searchString, $returnType) {
         $response = "";
         $requireParams = explode('&', $searchString);
-       
-        error_log("----------------------------");
-        error_log($searchString);
-        error_log(var_export($requireParams,true));
         $requireType = $this->getUserInput($requireParams[0]);
         if ($requireType == 'search') {
 
@@ -301,12 +297,10 @@ class Controller extends CController {
                 "bool": {
                     "must": [
                         {
-
                             "query_string": {
                                 "default_field": "couchbaseDocument.doc.' . $default_field . '",
                                 "query": " ' . $ids . ' "
                                                     }
-
                         }
                     ]
                 }

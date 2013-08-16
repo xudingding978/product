c4fe96497ed1dbab68451f09b8bebcdc4492c19c
Leo Sun <xsun051@hotmail.com>
2013-08-16 13:35:23 +1200

    dasd

--- app_hubstar/test/spec/test.js
+++ app_hubstar/test/spec/test.js
@@ -35,27 +35,7 @@ describe('Array', function(){
     });
   });
 });
-describe('Array', function(){
-  describe('testingggggggggggg', function(){
-    it('should return -1 when the value is not present', function(){
-      [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(0).should.equal(-1);
-    });
-  });
-});
-describe('Array', function(){
-  describe('testingggggggggggg', function(){
-    it('should return -1 when the value is not present', function(){
-      [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(0).should.equal(-1);
-    });
-  });
-});
-describe('Array', function(){
-  describe('testingggggggggggg', function(){
-    it('should return -1 when the value is not present', function(){
-      [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(0).should.equal(-1);
-    });
-  });
-});
+
+
+
+

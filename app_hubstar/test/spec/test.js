ca3ac4417aef373efc6d5be9ef5c5540dba2577e
Leo Sun <xsun051@hotmail.com>
2013-08-20 14:41:20 +1200

    ff

--- app_hubstar/test/spec/test.js
+++ app_hubstar/test/spec/test.js
@@ -1,11 +1,5 @@
 
-//
-//HubStar.Store = DS.Store.extend({
-//    revision: 12,
-//    adapter: DS.FixtureAdapter.create({ simulateRemoteResponse: false })
-//});
-//
-//
+
 //beforeEach(function() {
 //    Ember.run(function() {
 //        HubStar.reset();
@@ -17,56 +11,131 @@
 //    Ember.testing = false;
 //});
 //
-//after(function() {
-//    Ember.run(function() {
-//        HubStar.reset();
-//    });
-//});
+after(function(done) {
+    LoginOut();
+    Ember.run(function() {
+        route = HubStar.Router.create();
+        route.transitionTo("indexIndex");
+          done();
+    });
+
+});
+function LoginIn(done) {
+
+    if (localStorage.loginstatus === null || localStorage.loginstatus === undefined) {
+        $("a:contains('Login')").click();
+        $(".square-button").last().click();
 
 
-describe("Platform side bar ", function() {
+         setTimeout(function() {
+            var content = $('#login_iframe').contents();
+            content.find('#LoginForm_username').val('Jenny');
+            content.find('#LoginForm_password').val('test123');
+
+            content.find("input:submit.ifame_login").click();
+            done();
+        }, 1000);
+
+    }
+
+
+}
+;
+function LoginOut() {
+    $('.profilepic-container').find('a').eq(1).find('img').click();
+
+    //    $("a:contains('Logout')").click();
+
+     setTimeout(function() {
+        $("a:contains('Logout')").click();
+    }, 1000);
+
+
+}
+;
+
+
+describe("Platform  bar", function() {
     var controller;
-    var test;
+    var topics;
     var result;
-    beforeEach(function() {
+    beforeEach(function(done) {
+
+
+
         Ember.run(function() {
-            // Won't actually load until the end of the run-block.
+            LoginIn(done);
+
             controller = HubStar.PlatformBarController.create();
-            test = controller.categorys;
-
-
-            test.addObserver('isLoaded', function() {
-                if (test.get('isLoaded')) {
-                    result = test.get('length');
-                    console.log(result);
+            topics = controller.categorys;
+            topics.addObserver('isLoaded', function() {
+                if (topics.get('isLoaded')) {
+                    result = topics.get('length');
+                    done();
                 }
             });
+        });
+    });
+    it("total topics", function() {
+        Ember.run(function() {
+            result.should.equal(14);
+        });
+    });
+});
+describe("profiles Routing ", function() {
+    beforeEach(function(done) {
+        Ember.run(function() {
+            route = HubStar.Router.create();
+            route.transitionTo("profiles");
+             setTimeout(function() {
+                $('.nothingHere').find('ul').eq(0).find('a').click();
+                done();
+            }, 600);
 
         });
     });
-
-
-    it("Side bar catagories length", function() {
-
-
+    afterEach(function() {
         Ember.run(function() {
-            setTimeout(function() {
-                console.log(result);
-                result.should.equal(3);
-            }, 1000);
-
+            route = HubStar.Router.create();
+            route.transitionTo("searchIndex");
         });
-
-
+    });
+    it("Routing", function() {
+        Ember.run(function() {
+            //         $('#aside_contact').find('tr').eq(1).find('th').eq(0).should.have.text("Category:");
+        });
     });
 });
 
-describe('Array', function() {
-    describe('testingggggggggggg', function() {
-        it('should return -1 when the value is not present', function() {
-            [1, 2, 3].indexOf(5).should.equal(-1);
-            [1, 2, 3].indexOf(0).should.equal(-1);
+describe("testing platform side bar searching result", function() {
+    it("hover and click topics", function() {
+        $('.firstList').find('li').eq(1).find('a').eq(0).click();
+        $('.search_business').text().should.be($('.firstList').find('li').eq(1).find('a').eq(0).text().trim());
+
+    });
+
+    it("hover and click subtopics", function() {
+        $('.firstList').find('li').eq(1).find('ul').find('li').eq(1).find('ul').find('li').eq(1).find('a').click();
+        $('.search_business').text().should.be($('.firstList').find('li').eq(1).find('ul').find('li').eq(1).find('ul').find('li').eq(1).text().trim());
+    });
+});
+
+describe("user profies testing", function() {
+    var controller;
+
+    beforeEach(function() {
+
+        Ember.run(function() {
+            //           controller = HubStar.UserController.create();
+
         });
+    });
+    afterEach(function() {
+
+    });
+    it("user controller ", function() {
+        //     Ember.run(function() {
+
     });
 });
 

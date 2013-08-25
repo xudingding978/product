314d976ce7db26142d8d6a11ccb9d1bb50bbda5c
Leo Sun <xsun051@hotmail.com>
2013-08-26 10:00:02 +1200

    fff

--- app_hubstar/test/spec/test.js
+++ app_hubstar/test/spec/test.js
@@ -1,9 +1,71 @@
+
+
+//beforeEach(function() {
+//    Ember.run(function() {
+//        HubStar.reset();
+//    });
+//    Ember.testing = true;
+//});
+//
+//afterEach(function() {
+//    Ember.testing = false;
+//});
+//
+after(function(done) {
+    LoginOut();
+    Ember.run(function() {
+        route = HubStar.Router.create();
+        route.transitionTo("indexIndex");
+        done();
+    });
+
+});
+function LoginIn(done) {
+
+    if (localStorage.loginstatus === null || localStorage.loginstatus === undefined) {
+        $("a:contains('Login')").click();
+        $(".square-button").last().click();
+
+
+         setTimeout(function() {
+            var content = $('#login_iframe').contents();
+            content.find('#LoginForm_username').val('Jenny');
+            content.find('#LoginForm_password').val('test123');
+
+            content.find("input:submit.ifame_login").click();
+            done();
+        }, 1000);
+
+    }
+
+
+}
+;
+function LoginOut() {
+    $('.profilepic-container').find('a').eq(1).find('img').click();
+
+    //    $("a:contains('Logout')").click();
+
+     setTimeout(function() {
+        $("a:contains('Logout')").click();
+    }, 1000);
+
+
+}
+;
+
+
 describe("Platform  bar", function() {
     var controller;
     var topics;
     var result;
     beforeEach(function(done) {
+
+
+
         Ember.run(function() {
+            LoginIn(done);
+
             controller = HubStar.PlatformBarController.create();
             topics = controller.categorys;
             topics.addObserver('isLoaded', function() {
@@ -15,41 +77,35 @@ describe("Platform  bar", function() {
         });
     });
     it("total topics", function() {
-        $(".profilepic_user").click();
-
-
-         setTimeout(function() {
-            controller = HubStar.UserController.create();
-            controller.userDashboardButton();
-            done();
-        }, 600);
         Ember.run(function() {
-            //           controller = HubStar.UserController.create();
+            result.should.equal(14);
+        });
+    });
 });
-//describe("profiles Routing ", function() {
-//    beforeEach(function(done) {
-//        Ember.run(function() {
-//            route = HubStar.Router.create();
-//            route.transitionTo("profiles");
-//            setTimeout(function() {
-//                $('.nothingHere').find('ul').eq(0).find('a').click();
-//                done();
-//            }, 600);
-//
-//        });
-//    });
-//    afterEach(function() {
-//        Ember.run(function() {
-//            route = HubStar.Router.create();
-//            route.transitionTo("searchIndex");
-//        });
-//    });
-//    it("Routing", function() {
-//        Ember.run(function() {
-//            $('#aside_contact').find('tr').eq(1).find('th').eq(0).should.have.text("Category:");
-//        });
-//    });
-//});
+describe("profiles Routing ", function() {
+    beforeEach(function(done) {
+        Ember.run(function() {
+            route = HubStar.Router.create();
+            route.transitionTo("profiles");
+             setTimeout(function() {
+                $('.nothingHere').find('ul').eq(0).find('a').click();
+                done();
+            }, 600);
+
+        });
+    });
+    afterEach(function() {
+        Ember.run(function() {
+            route = HubStar.Router.create();
+            route.transitionTo("searchIndex");
+        });
+    });
+    it("Routing", function() {
+        Ember.run(function() {
+            //         $('#aside_contact').find('tr').eq(1).find('th').eq(0).should.have.text("Category:");
+        });
+    });
+});
 
 describe("testing platform side bar searching result", function() {
     it("hover and click topics", function() {
@@ -64,17 +120,24 @@ describe("testing platform side bar searching result", function() {
     });
 });
 
-describe("test user profile flick button", function() {
-    it("test user profile flick button", function() {
-        Ember.run(function() {
-               platformBarController = HubStar.PlatformBarController.create();
-               console.log(platformBarController);
-            var thiscontroller = HubStar.UserController.create();
-            console.log(thiscontroller);
-            thiscontroller.flickButtonClick();
-            thiscontroller.get("isEditingMode").should.equal(true);
-        });
+describe("user profies testing", function() {
+    var controller;
 
+    beforeEach(function() {
+
+        Ember.run(function() {
+            //           controller = HubStar.UserController.create();
+
+        });
+    });
+    afterEach(function() {
+
+    });
+    it("user controller ", function() {
+        //     Ember.run(function() {
+
+    });
+});
 
 
 describe("poster picture  testing", function() {
@@ -146,3 +209,5 @@ describe("poster photo_url  validation  testing", function() {
         route.transitionTo("searchIndex");
     });
 });
+
+

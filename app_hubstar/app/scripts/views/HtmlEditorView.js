3ee7445d317f3b779d56ddb501db834165c14828
Charis Wan <wanjiang1990@gmail.com>
2013-09-23 08:07:00 +1200

    pull into developssss

--- app_hubstar/app/scripts/views/HtmlEditorView.js
+++ app_hubstar/app/scripts/views/HtmlEditorView.js
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-
-
-=======
->>>>>>> ae58bbce6e20d06064e30b4f2a9ceb464e45715e
 HubStar.HtmlEditorView = Ember.View.extend({
     templateName: 'htmlEditor',
-    
-    
-<<<<<<< HEAD
-    
-    
-    
-=======
->>>>>>> ae58bbce6e20d06064e30b4f2a9ceb464e45715e
+
 });

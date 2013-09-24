97b7b0ac9dc95fd11de95989fd53468325b9f95f
Client Lewis <clint.lewis@trendsideas.com>
2013-09-24 13:08:07 +1200

    css style commit

--- app_hubstar/app/scripts/controllers/UserController.js
+++ app_hubstar/app/scripts/controllers/UserController.js
@@ -295,11 +295,11 @@ HubStar.UserController = Ember.Controller.extend({
             this.set('editingInterest', !this.get('editingInterest'));
         
         } 
-//        else if (checkingInfo === "aboutMe") {
-//            about_record = data;
-//            this.set('editingAbout', !this.get('editingAbout'));
-//           
-//        }
+        else if (checkingInfo === "aboutMe") {
+            about_record = data;
+            this.set('editingAbout', !this.get('editingAbout'));
+           
+        }
     },
     yes: function(checkingInfo) {
         if (checkingInfo === "interest") {
@@ -315,10 +315,10 @@ HubStar.UserController = Ember.Controller.extend({
             this.set('interests', interest_record);
             this.set('editingInterest', !this.get('editingInterest'));
         } 
-//        else if (checkingInfo === "aboutMe") {
-//            this.set('about_me', about_record);
-//            this.set('editingAbout', !this.get('editingAbout'));
-//        }
+        else if (checkingInfo === "aboutMe") {
+            this.set('about_me', about_record);
+            this.set('editingAbout', !this.get('editingAbout'));
+        }
 
     },
     submit: function()
@@ -372,7 +372,7 @@ HubStar.UserController = Ember.Controller.extend({
             update_user_record.set('display_name', this.get('display_name'));
             update_user_record.set('first_name', this.get('first_name'));
             update_user_record.set('last_name', this.get('last_name'));
-             update_user_record.set('about_me', this.get('about_me'));
+          
             this.saveLink('facebook_link', 'facebook'); 
             this.saveLink('twitter_link','twitter');
             this.saveLink('googleplus_link', 'googleplus');
@@ -415,8 +415,8 @@ HubStar.UserController = Ember.Controller.extend({
         var last_name = new checkObject("last_name", this.get('last_name'), 128, null, null, null);
         checkList.push(last_name);
         
-        var about_me = new checkObject("about_me", this.get('about_me'), 4096, null, null, null);
-        checkList.push(about_me);
+//        var about_me = new checkObject("about_me", this.get('about_me'), 4096, null, null, null);
+//        checkList.push(about_me);
         var location = new checkObject("location", this.get('location'), 128, null, null, null);
         checkList.push(location);
         var facebook = new checkObject("facebook", this.get('facebook'), 128, true, null, "facebook");
@@ -522,7 +522,7 @@ if (checkList[i].id === 'first_name' || checkList[i].id === 'last_name')
             }
             this.set('interests', tempInterest.substring(1, tempInterest.length));
             update_interest_record.set('selected_topics', this.get('interests'));
-             // update_interest_record.set('about_me', editor.getValue());
+              update_interest_record.set('about_me', editor.getValue());
             HubStar.store.save();
         } else {
             this.get('controllers.applicationFeedback').statusObserver(null, "invalid input");

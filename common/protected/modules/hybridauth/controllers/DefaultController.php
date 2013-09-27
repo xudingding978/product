e2a5d52a8b2e5806a93f59d2876b07568dccfa46
dingding_xu <dingding@hubstar.co>
2013-09-27 16:47:19 +1200

    #235 get back cb connect

--- common/protected/modules/hybridauth/controllers/DefaultController.php
+++ common/protected/modules/hybridauth/controllers/DefaultController.php
@@ -178,7 +178,7 @@ class DefaultController extends CController {
         $userProfile->save();
 
 
-        $cb = new Couchbase("cb1.hubsrv.com:8091", "", "", "production", true);
+        $cb = new Couchbase("cb1.hubsrv.com:8091", "Administrator", "Pa55word", "production", true);
         $rand_id = $user->COUCHBASE_ID;
         $temp = $this->getMega();
         $temp["id"] = $rand_id;

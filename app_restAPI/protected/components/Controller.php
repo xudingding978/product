f45c28e35bc8db060fa778e2c4e479c11f67b660
Sean_Xu <sean@hubstar.co>
2013-10-04 16:47:00 +1300

    updated to removed the boost until we sort this better

--- app_restAPI/protected/components/Controller.php
+++ app_restAPI/protected/components/Controller.php
@@ -227,7 +227,7 @@ class Controller extends CController {
         $request = $this->getElasticSearch();
         $request->from($from);
         $request->size($size);
-        $sortArray=array('couchbaseDocument.doc.boost','couchbaseDocument.doc.created');
+        $sortArray=array('couchbaseDocument.doc.created');
         $length = sizeof($sortArray);
         for ($i = 0; $i < $length; $i++) {
             $sort = $this->getsortQuestWithQueryString($sortArray[$i]);

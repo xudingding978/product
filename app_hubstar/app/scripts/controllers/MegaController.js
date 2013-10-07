7778978bfc674f933814a2586498530602cfda8b
Sean_Xu <sean@hubstar.co>
2013-10-08 11:51:33 +1300

    #453 display the right user picture in photo and article page

--- app_hubstar/app/scripts/controllers/MegaController.js
+++ app_hubstar/app/scripts/controllers/MegaController.js
@@ -155,7 +155,7 @@ HubStar.MegaController = Ember.ArrayController.extend({
         var commentContent = this.get('commentContent');
         if (commentContent) {
             var comments = this.get('megaResouce').get('comments');
-            var commenter_profile_pic_url = this.get("currentUser").get('photo_url');
+            var commenter_profile_pic_url = this.get("currentUser").get('photo_url_large');
             var commenter_id = this.get("currentUser").get('id');
             var name = this.get("currentUser").get('display_name');
             var date = new Date();

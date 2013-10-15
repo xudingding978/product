ed96e895fbd517dab9317cf4051324eb084b3816
Tom_Lin <tom@hubstar.co>
2013-10-15 14:34:14 +1300

    finish the message route implementation

--- app_hubstar/app/scripts/routes/UserMessageRoute.js
+++ app_hubstar/app/scripts/routes/UserMessageRoute.js
@@ -5,14 +5,6 @@
 
 
 HubStar.UserMessageRoute = Ember.Route.extend({
-//          model: function(params) {
-//
-//            this.controllerFor('user').set('switchPhoto', false);
-//            var address = document.URL;
-//            var user_id = address.split("#")[1].split("/")[2];
-//              this.controllerFor('message').set("model", model);
-//            return HubStar.Mega.find({RquireType: "personalCollection", user_id: user_id, collection_id: params.collection_id});
-//        },
     setupController: function(controller, model) {
 
         this.controllerFor('user').set('profileSelectionStatus', 'Messages');
@@ -21,29 +13,14 @@ HubStar.UserMessageRoute = Ember.Route.extend({
         this.controllerFor('user').set('followerTag', false);
         this.controllerFor('user').set('followingTag', false);
         this.controllerFor('user').set('messageTag', true);
-//            console.log(this.controllerFor('checkAuthorityStatus').);
- //      this.controllerFor('userMessage').set("model", model);
-    this.controllerFor('userMessage').setUserMessage(model);
-        console.log(model);
+        this.controllerFor('userMessage').setUserMessage(model);
 
         $(window).scrollTop(0);
     },
-       model: function(params) {
+    model: function(params) {
 
         var address = document.URL;
         var user_id = address.split("#")[1].split("/")[2];
-return user_id;
-//        var address = document.URL;
-//        var user_id = address.split("#")[1].split("/")[2];
-//         var model =  HubStar.User.find(params.user_id);
-//         var message =  model.get("messages");
-//         message.addObserver('isLoaded', function() {
-//    if (message.get('isLoaded')) {
-//             return message;
-//    }
-//});
-     
-       // return HubStar.Mega.find({RquireType: "personalCollection", user_id: user_id, message_id: "54813817093857045634432307356344323073"});
-     
+        return user_id;
     }
 });

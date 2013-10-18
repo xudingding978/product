fe9be0793a51b99c9129e477e29757ed61bccfdb
Charis_Wan <charis@hubstar.co>
2013-10-18 15:02:32 +1300

    star

--- app_hubstar/app/scripts/views/ReviewView .js
+++ app_hubstar/app/scripts/views/ReviewView .js
@@ -1,33 +1,29 @@
 HubStar.ReviewView = Ember.View.extend({
-  
-    
-    
     templateName: 'review',
     didInsertElement: function() {
-      
-  }
-          
-           
-//            
-//            
-//            evaluationCancel: function() {
-//        var controller = this.get('controller');
-//        if (controller._debugContainerKey.indexOf("profile") !== -1) {
-//
-//
-//            controller.cancelDelete();
-//
-//        }
-//        
-//
-//
-//    }
-    
-    
-    
-    
-    
-    
 
+        $(".star-rating").mouseover(function() {
+            $(this).addClass('star-rating-hover star-rating-applied');
+
+        });
+        $(".star-rating").mouseout(function() {
+            $(this).removeClass('star-rating-hover');
+            //$(this).addClass('star-rating-on');
+
+        });
+        $(".star-rating").click(function() {
+            $(this).removeClass('star-rating-hover');
+            $(this).addClass('star-rating-on');
+
+        });
+        $(".rating-cancel").click(function() {
+            $(".star-rating").removeClass('star-rating-on');
+
+        });
+
+
+    }
+    
+    
 
 });

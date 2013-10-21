2189fab4e58a700db1614013e0e79ed5cabb2686
Shuai_Yang <shuai@hubstar.co>
2013-10-22 10:43:59 +1300

    #532  implement  character limit

--- app_hubstar/app/scripts/controllers/CollectionController.js
+++ app_hubstar/app/scripts/controllers/CollectionController.js
@@ -68,8 +68,9 @@ HubStar.CollectionController = Ember.Controller.extend({
 
         var desc = selectedCollection.get('desc');
 
-        
+        console.log(selectedCollection.get('desc').length);
         if (selectedCollection.get('desc') !== undefined) {
+            
 
             desc = selectedCollection.get("desc");
       }

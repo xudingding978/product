cac7166d30d0662b1deb7dbec49477fe4ca3c6ab
Shuai_Yang <shuai@hubstar.co>
2013-10-22 09:34:17 +1300

    checkout to anthoer branch

--- app_hubstar/app/scripts/views/ReviewView .js
+++ app_hubstar/app/scripts/views/ReviewView .js
@@ -6,59 +6,32 @@ HubStar.ReviewView = Ember.View.extend({
      
    $(document).ready(function() {
   $('#example-1').ratings(10).bind('ratingchanged', function(event, data) {
-      
-
-    $('#example-rating-1').text(data.rating);
-
-    
-  });
- 
+  $('#example-rating-1').text(data.rating); 
+  }); 
 });
         
 
-        $(".star-rating").mouseover(function() {
-            $(this).addClass('star-rating-hover star-rating-applied');
-
-        });
-        $(".star-rating").mouseout(function() {
-            $(this).removeClass('star-rating-hover');
-            //$(this).addClass('star-rating-on');
-
-        });
-        $(".star-rating").click(function() {
-            $(this).removeClass('star-rating-hover');
-            $(this).addClass('star-rating-on');
-            
-        });
-        $(".rating-cancel").click(function() {
-            $(".star-rating").removeClass('star-rating-on');
-
-        });
+//        $(".star-rating").mouseover(function() {
+//            $(this).addClass('star-rating-hover star-rating-applied');
+//
+//        });
+//        $(".star-rating").mouseout(function() {
+//            $(this).removeClass('star-rating-hover');
+//            //$(this).addClass('star-rating-on');
+//
+//        });
+//        $(".star-rating").click(function() {
+//            $(this).removeClass('star-rating-hover');
+//            $(this).addClass('star-rating-on');
+//            
+//        });
+//        $(".rating-cancel").click(function() {
+//            $(".star-rating").removeClass('star-rating-on');
+//
+//        });
 
     
-//        $('.star-rating').rating({
-//            focus: function(value, link) {
-//                // 'this' is the hidden form element holding the current value
-//                // 'value' is the value selected
-//                // 'element' points to the link element that received the click.
-//                var tip = $('#hover-test');
-//                tip[0].data = tip[0].data || tip.html();
-//              
-//                tip.html(link.title || 'value: ' + value);
-//                 
-//                
-//                var value=link.title;
-//             
-//            },
-//            blur: function(value, link) {
-//                var tip = $('#hover-test');
-//              
-//                $('#hover-test').html(tip[0].data || '');
-//                    
-//           //   console.log(link.title);
-//                
-//            }
-//        });
+
    
 
     }

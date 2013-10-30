de5c64f31210be3032048d5b73c10c6d422e6840
Kingsley_Huang <kingsley@hubstar.co>
2013-10-31 11:04:28 +1300

    megaCreateController is add

--- app_hubstar/app/scripts/controllers/AddVideoController.js
+++ app_hubstar/app/scripts/controllers/AddVideoController.js
@@ -1,10 +1,16 @@
 
 HubStar.AddVideoController = Ember.ObjectController.extend({
-    needs: ["profileVideos", "applicationFeedback"],
+    needs: ["profileVideos", "applicationFeedback", 'profile','megaCreate'],
     videoUrl: null,
     videoImg: null,
     videoTitle: null,
     videoDesc: null,
+    videoid: null,
+    profileMega: null,
+    init: function() {
+        this.setMega();
+        console.log(this.get("profileMega"));
+    },
     canel: function() {
         var profileVideoController = this.get('controllers.profileVideos');
         this.reset();
@@ -12,11 +18,12 @@ HubStar.AddVideoController = Ember.ObjectController.extend({
     },
     getVideoFromYoutube: function()
     {
-        var video_id = this.getVideoId();
+
+        this.set('videoid', this.getVideoId());
         var that = this;
-        if (video_id !== null) {
+        if (this.get('videoid') !== null) {
             $.ajax({
-                url: "http://gdata.youtube.com/feeds/api/videos/" + video_id + "?v=2&alt=jsonc",
+                url: "http://gdata.youtube.com/feeds/api/videos/" + this.get('videoid') + "?v=2&alt=jsonc",
                 type: 'get',
                 success: function(feedback) {
                     that.set('videoImg', feedback.data.thumbnail.hqDefault);
@@ -33,16 +40,19 @@ HubStar.AddVideoController = Ember.ObjectController.extend({
         this.set('videoUrl', null);
         this.set('videoTitle', null);
         this.set('videoDesc', null);
+        this.set('videoid', null);
+
     },
     videoCreate: function() {
+
+
     },
     getVideoId: function() {
-        //  this.set('videoIframeCode', this.get("videoUrl"));
         var videoid = null;
         var videoUrl = this.get("videoUrl");
         if (videoUrl.indexOf("http://www.youtube.com/") !== -1)
         {
-            var tmpId = videoUrl.split("=");
+            var tmpId = videoUrl.split("v=");
             videoid = tmpId[1];
         }
         else if (videoUrl.indexOf("http://youtu.be/") !== -1)
@@ -54,8 +64,20 @@ HubStar.AddVideoController = Ember.ObjectController.extend({
     },
     getIframeCode: function(width, height, videoid)
     {
-        var tempCode = '<iframe width="' + width + '" height="' + height + '" src="//www.youtube.com/embed/' + videoid + '" frameborder="0" allowfullscreen></iframe>';
-        return tempCode;
-        //     this.set('videoIframeCode', tempCode);
+        var iframeCode = '<iframe width="' + width + '" height="' + height + '" src="//www.youtube.com/embed/' + videoid + '" frameborder="0" allowfullscreen></iframe>';
+        return iframeCode;
+    },
+    setMega: function() {
+        var profileController = this.get('controllers.profile');
+        var tempmega = profileController.get("model");
+        var that = this;
+        that.set("profileMega", tempmega);
+        if (that.get("profileMega") === null) {
+            tempmega.addObserver('isLoaded', function() {
+                if (tempmega.get('isLoaded')) {
+                    that.set("profileMega", tempmega);
+                }
+            });
+        }
     }
 });

1d0dd5535ebf7c8778ab5ab89e6afacc4da0b1d2
Charis_Wan <charis@hubstar.co>
2013-11-07 07:39:21 +1300

    google map

--- app_hubstar/app/scripts/views/GoogleMapPopupView.js
+++ app_hubstar/app/scripts/views/GoogleMapPopupView.js
@@ -1,5 +1,7 @@
 HubStar.GoogleMapPopupView = Ember.View.extend({
     templateName: 'googleMapPopup',
+   // routeModeDropdown: false,
+    routeModeSelection: "Driving",
     didInsertElement: function() {
         var directionsDisplay = new google.maps.DirectionsRenderer({draggable: true});
         var directionsService = new google.maps.DirectionsService();
@@ -41,14 +43,25 @@ HubStar.GoogleMapPopupView = Ember.View.extend({
                 that.get('controller').set('popUpMap', false);
             });
 
+ $('#routeModeDropdown > .ite').click(function() {
+            console.log("2");
+            that.set('routeModeSelection', $(this).text());    
+            console.log("3");
+            that.set('routeModeDropdown',false);
+            console.log("4");
+        });
+
+
         });
 
         var thatthat = that;
+        console.log(thatthat.get('routeModeSelection'));
         function calcRoute() {
             var request = {
                 origin: thatthat.get('controller').get('fromAddress'),
                 destination: thatthat.get('controller').get('toAddress'),
-                travelMode: google.maps.DirectionsTravelMode[$("#routeMode").val()]
+                 
+                travelMode: google.maps.DirectionsTravelMode[$("#routeModeDropdown >,ite").text()]
             };
             directionsService.route(request, function(response, status) {
                 if (status === google.maps.DirectionsStatus.OK) {
@@ -78,22 +91,23 @@ HubStar.GoogleMapPopupView = Ember.View.extend({
                 });
             }, 800);
         }
+    },
+    dropdownRoute: function() {
+        $("#routeModeDropdown").toggleClass('hideClass');
+    },
+    selectRoute: function() {
+        var that = this;    
+        console.log("1");
+        $('#routeModeDropdown > .ite').click(function() {
+            console.log("2");
+            that.set('routeModeSelection', $(this).text());  
+
+            console.log("4");
+        });
+        $("#routeModeDropdown").toggleClass('hideClass');
+        console.log("5");
+       // 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 });

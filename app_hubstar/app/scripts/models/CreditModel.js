ce042ad5ab7beed0ebf4ba4c718ec66379c25277
Sean_Xu <sean@hubstar.co>
2013-11-07 11:41:28 +1300

    #585 merge develop

--- app_hubstar/app/scripts/models/CreditModel.js
+++ app_hubstar/app/scripts/models/CreditModel.js
@@ -1,5 +1,5 @@
 HubStar.Credit = DS.Model.extend({
-    credit_id: DS.attr('string'),
+    credit_list_id: DS.attr('string'),
     credit_list_name: DS.attr('string'),
     credit_list_text: DS.attr('string'),
     credit_list_credit_link_category: DS.attr('string'),

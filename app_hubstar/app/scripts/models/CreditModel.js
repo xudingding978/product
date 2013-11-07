3a20b76fa547c1f3e91876f72f451b6ff4aba6b4
Sean_Xu <sean@hubstar.co>
2013-11-07 13:31:08 +1300

    #585 change name of credit model according to ray

--- app_hubstar/app/scripts/models/CreditModel.js
+++ app_hubstar/app/scripts/models/CreditModel.js
@@ -1,9 +1,9 @@
 HubStar.Credit = DS.Model.extend({
-    credit_list_id: DS.attr('string'),
-    credit_list_name: DS.attr('string'),
-    credit_list_text: DS.attr('string'),
-    credit_list_credit_link_category: DS.attr('string'),
-    credit_list_credit_link_subcategory: DS.attr('string'),
+    credits_id: DS.attr('string'),
+    credits_name: DS.attr('string'),
+    credits_text: DS.attr('string'),
+    credits_category_name: DS.attr('string'),
+    credits_sub_category_name: DS.attr('string'),
     optional:DS.attr('string'),
     didLoad: function() {
     }

c1172d516435d7fd77e871bc969842970fada5e8
Client Lewis <clint.lewis@trendsideas.com>
2013-11-20 11:22:52 +1300

    #617 - repositioning of when the top nav search appears

--- app_hubstar/app/scripts/views/ApplicationView.js
+++ app_hubstar/app/scripts/views/ApplicationView.js
@@ -9,7 +9,7 @@ HubStar.ApplicationView = Ember.View.extend({
 
         $(".navbar").css("background", " url(../../images/landingpagebg.jpg)");
        
-        var scroll_pos_test = 440;
+        var scroll_pos_test = 290;
 
         $(document).ready(function() {
             $(window).scroll(function() {

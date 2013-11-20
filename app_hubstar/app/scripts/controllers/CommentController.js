637f498ac7d018b7b3fa8bb38d3deaaf0276e13b
Sean_Xu <sean@hubstar.co>
2013-11-21 08:13:55 +1300

    #649 image data stream bug fix

--- app_hubstar/app/scripts/controllers/CommentController.js
+++ app_hubstar/app/scripts/controllers/CommentController.js
@@ -60,7 +60,8 @@ HubStar.CommentController = Ember.Controller.extend({
         if (commentContent) {
             var comments = this.get('mega').get('comments');
 
-            var commenter_profile_pic_url = this.get("currentUser").get('photo_url_large');
+//            var commenter_profile_pic_url = this.get("currentUser").get('photo_url_large');
+            var commenter_profile_pic_url = HubStar.get('photoDomain') + '/users/' + localStorage.loginStatus +'/user_picture/user_picture';
             var commenter_id = this.get("currentUser").get('id');
             var name = this.get("currentUser").get('display_name');
             var date = new Date();

89c606da17f559fa86d1c2a3da8c52f19483d447
dingding_xu <dingding@hubstar.co>
2013-11-25 16:31:17 +1300

    #643 scroll bar half

--- app_hubstar/app/scripts/views/CommentView.js
+++ app_hubstar/app/scripts/views/CommentView.js
@@ -2,6 +2,26 @@ HubStar.CommentView = Ember.View.extend({
     templateName: 'comment',
     didInsertElement: function() {
         $('.user_comment_' + localStorage.loginStatus).attr('style', 'display:block');
+          var that =this;
+        $(document).ready(function() { 
+            
+            $("#commentScrollBar_" + that.get("controller").get('model').get('id')).mCustomScrollbar({
+                scrollButtons: {
+                    enable: false,
+                    scrollSpeed: "auto"
+                },
+                advanced: {
+                    updateOnBrowserResize: true,
+                    updateOnContentResize: true,
+                    autoScrollOnFocus: false,
+                    normalizeMouseWheelDelta: false
+                },
+                autoHideScrollbar: true,
+                mouseWheel: true,
+                theme: "dark-2",
+                set_height: 300
+            });
+        });
     },
     openComment: function(id) {
 

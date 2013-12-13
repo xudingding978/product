e1dd3f94440513b360dcea347bc93356b0053eaf
Shuai_Yang <shuai@hubstar.co>
2013-12-13 16:30:19 +1300

    #727 checking for cache localstorage sessionstorage webworker websocket pushstate

--- app_hubstar/app/bower_components/javascriptHelper/browserdetecter.js
+++ app_hubstar/app/bower_components/javascriptHelper/browserdetecter.js
@@ -1,15 +1,19 @@
+// feature detection script to check that the browser support the baseline requirements for the platform
 
-var browerAllow=false;
-if (Modernizr.applicationcache&&Modernizr.localstorage&&Modernizr.history)
+var browserAllow=false;
+
+// checking for Application Cache, HTML5 LocalStorage, SessionStorage, Web Workers, Web Sockets and pushState 
+if (Modernizr.applicationcache&&Modernizr.localstorage&&Modernizr.history&&Modernizr.webworkers&&Modernizr.sessionstorage&&Modernizr.websockets)
 {
-    browerAllow = true;
+    browserAllow = true;
 }else{
-    browerAllow = false;
+    browserAllow = false;
 }
 
-
-if (!browerAllow) {
+// failed feature detection, redirecting to the Upgrade page
+if (!browserAllow) {
     var api_url = document.domain;
     window.location = "http://" + api_url + "/upgrade.html";
-
 }
+
+

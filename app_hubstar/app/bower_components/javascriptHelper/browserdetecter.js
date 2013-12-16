ab3dd7dd153ff1f3446e974cc407f299558c44ca
Shuai_Yang <shuai@hubstar.co>
2013-12-17 11:31:11 +1300

    test without webworker

--- app_hubstar/app/bower_components/javascriptHelper/browserdetecter.js
+++ app_hubstar/app/bower_components/javascriptHelper/browserdetecter.js
@@ -3,7 +3,7 @@
 var browserAllow=false;
 
 // checking for Application Cache, HTML5 LocalStorage, SessionStorage, Web Workers, Web Sockets and pushState 
-if (Modernizr.applicationcache&&Modernizr.localstorage&&Modernizr.history&&Modernizr.webworkers&&Modernizr.sessionstorage&&Modernizr.websockets)
+if (Modernizr.applicationcache&&Modernizr.localstorage&&Modernizr.history&&Modernizr.sessionstorage&&Modernizr.websockets)
 {
     browserAllow = true;
 }else{

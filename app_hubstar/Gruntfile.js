bbd209d1fb8e19ba1c14d4df298accf89f8332e5
Sean_Xu <sean@hubstar.co>
2013-12-17 15:56:49 +1300

    #641 about us mac can not select HTML5 or Template bug fix

--- app_hubstar/Gruntfile.js
+++ app_hubstar/Gruntfile.js
@@ -122,7 +122,7 @@ module.exports = function(grunt) {
             options: {
                 port: 9000,
                 // change this to '0.0.0.0' to access the server from outside
-                hostname: '0.0.0.0'
+                hostname: 'localhost'
             },
             livereload: {
                 options: {

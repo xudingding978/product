2d6253400f7f0839d62b4e98e0e13ffce7fc6e98
dingding_xu <dingding@hubstar.co>
2013-12-18 10:50:55 +1300

    1.2

--- app_hubstar/app/scripts/views/SearchAreaTextFieldView.js
+++ app_hubstar/app/scripts/views/SearchAreaTextFieldView.js
@@ -2,26 +2,26 @@ HubStar.SearchAreaTextFieldView = Ember.TextField.extend({
 
         insertNewline: function() {
             var controller = this.get('targetObject');
-            if (controller.valueBinding._from.indexOf("addCollection") !== -1)
+            if (controller._debugContainerKey.indexOf("addCollection") !== -1)
             {
                 //this.get("controller").get("controllers.addCollection").addNewCollection(s);
                 controller.addNewCollection();
             }
-            else if (controller.valueBinding._from.indexOf("application") !== -1)
+            else if (controller._debugContainerKey.indexOf("application") !== -1)
             {
                 //this.get("controller").get("controllers.application").newSearch(s);
                 controller.newSearch();
             }
-            else if (controller.valueBinding._from.indexOf("mega") !== -1) {
+            else if (controller._debugContainerKey.indexOf("mega") !== -1) {
                 //this.get("controller").get("controllers.mega").addComment(s);
                 controller.addComment();
             }
-            else if (controller.valueBinding._from.indexOf("comment") !== -1) {
+            else if (controller._debugContainerKey.indexOf("comment") !== -1) {
             var s = this.$().parents()[0].id.split("_")[1];
             //this.get("controller").get("controllers.comment").openComment(s);
             controller.openComment(s);
         } 
-            else if (controller.valueBinding._from.indexOf("contact") !== -1) {
+            else if (controller._debugContainerKey.indexOf("contact") !== -1) {
                 //this.get("controller").get("controllers.contact").setEditable("Email");
                 controller.setEditable("Email");
             }

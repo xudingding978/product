acaf028558b8ecb3f02d7cd33c570371c1b8b805
dingding_xu <dingding@hubstar.co>
2013-12-18 09:20:37 +1300

    rollback

--- app_hubstar/app/scripts/views/SearchAreaTextFieldView.js
+++ app_hubstar/app/scripts/views/SearchAreaTextFieldView.js
@@ -4,26 +4,26 @@ HubStar.SearchAreaTextFieldView = Ember.TextField.extend({
             var controller = this.get('targetObject');
             if (controller.valueBinding._from.indexOf("addCollection") !== -1)
             {
-                this.get("controller").get("controllers.addCollection").addNewCollection(s);
-                //controller.addNewCollection();
+                //this.get("controller").get("controllers.addCollection").addNewCollection(s);
+                controller.addNewCollection();
             }
             else if (controller.valueBinding._from.indexOf("application") !== -1)
             {
-                this.get("controller").get("controllers.application").newSearch(s);
-                //controller.newSearch();
+                //this.get("controller").get("controllers.application").newSearch(s);
+                controller.newSearch();
             }
             else if (controller.valueBinding._from.indexOf("mega") !== -1) {
-                this.get("controller").get("controllers.mega").addComment(s);
+                //this.get("controller").get("controllers.mega").addComment(s);
                 controller.addComment();
             }
             else if (controller.valueBinding._from.indexOf("comment") !== -1) {
             var s = this.$().parents()[0].id.split("_")[1];
-            this.get("controller").get("controllers.comment").openComment(s);
-            //controller.openComment(s);
+            //this.get("controller").get("controllers.comment").openComment(s);
+            controller.openComment(s);
         } 
             else if (controller.valueBinding._from.indexOf("contact") !== -1) {
-                this.get("controller").get("controllers.contact").setEditable("Email");
-                //controller.setEditable("Email");
+                //this.get("controller").get("controllers.contact").setEditable("Email");
+                controller.setEditable("Email");
             }
             else {
        

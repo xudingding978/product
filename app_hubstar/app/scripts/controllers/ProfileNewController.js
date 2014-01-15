f1e6edfb396cad560d1c0e07adb599c63131ebd7
dingding_xu <dingding@hubstar.co>
2014-01-15 15:18:55 +1300

    profile builder finally & photo upload and refresh

--- app_hubstar/app/scripts/controllers/ProfileNewController.js
+++ app_hubstar/app/scripts/controllers/ProfileNewController.js
@@ -87,7 +87,7 @@ HubStar.ProfileNewController = Ember.Controller.extend({
                 && this.validateEmail($('.contactEmail').val())
                 && this.validateEmail($('.admins').val())
                 && this.validateEmail($('.clientEmail').val())
-       && this.numberChecking("#numberFormat", $('.contactNumber').val())
+             //   && this.numberChecking("#numberFormat", $('.contactNumber').val())
                 )
 
         {
@@ -389,12 +389,12 @@ HubStar.ProfileNewController = Ember.Controller.extend({
                 profile_website_url: this.get("website_url"),
                 profile_website: this.get("website"),
                 keywords: []
-               
+
             });
 
 
             newMegaNewModel.get("profile").pushObject(newProfile);
-            
+
             var that = this;
             requiredBackEnd('meganews', 'createNewProfile', newMegaNewModel, 'POST', function(params) {
                 if (params === true) {
@@ -404,9 +404,9 @@ HubStar.ProfileNewController = Ember.Controller.extend({
                     newMegaNewModel.store.save();
                     newMegaNewModel.addObserver('isDirty', function() {
                         if (!newMegaNewModel.get('isDirty')) {
-                            that.transitionToRoute('profile', newProfile);
-                            //location.href = "#/profiles/" + that.get("profile_url");
-                            //location.reload();
+                            //that.transitionToRoute('profile', newProfile);
+                            location.href = "#/profiles/" + that.get("profile_url");
+                            location.reload();
                         } else {
                         }
 

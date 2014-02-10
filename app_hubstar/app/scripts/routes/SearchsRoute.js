ccad6df93b37599bf6e68941a7105148101daec5
Shuai_Yang <shuai@hubstar.co>
2014-02-11 11:34:04 +1300

    #792 get rid of %20 and share search to public

--- app_hubstar/app/scripts/routes/SearchsRoute.js
+++ app_hubstar/app/scripts/routes/SearchsRoute.js
@@ -52,7 +52,7 @@ HubStar.SearchsRoute = Ember.Route.extend({
     redirect: function() {
 
         if (localStorage.getItem("loginStatus") === null || (localStorage.loginStatus === "")) {
-            this.transitionTo('indexIndex');
+//            this.transitionTo('indexIndex');
 
         } else {
             // this.transitionTo('searchIndex');

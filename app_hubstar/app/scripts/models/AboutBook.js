39013c76f6ffbd99e7ad918dbec14d97b72da1de
sean_xu <sean@hubstar.co>
2014-02-12 15:53:15 +1300

    #813 about us modification

--- app_hubstar/app/scripts/models/AboutBook.js
+++ app_hubstar/app/scripts/models/AboutBook.js
@@ -9,6 +9,14 @@ HubStar.AboutBook = DS.Model.extend({
     buy_available: DS.attr('boolean'),
     optional: DS.attr('string'),
     display_size: DS.attr('number'),
+    getSize: function() {
+        var size = false;        
+        if (this.get('display_size') != 0) {
+            console.log('111111');
+            size = true;
+        }
+        return size;
+    }.property('display_size'),
     didLoad: function() {
 
     }
--- app_hubstar/app/scripts/views/RadioButtonView.js
+++ app_hubstar/app/scripts/views/RadioButtonView.js
@@ -3,13 +3,9 @@ Ember.RadioButton = Ember.View.extend({
     type : "radio",
     attributeBindings : [ "name", "type", "value", "checked:checked:" ],
     click : function() {
-        console.log('222222');
         this.set("selection", this.$().val());
     },
     checked : function() {
-        console.log('3333333');
-        console.log(this.get('value'));
-        console.log(this.get('selection'));
-        return this.get("value") === this.get("selection");   
+        return this.get("value") == this.get("selection");   
     }.property()
 });

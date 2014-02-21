22457c92d6d4960e91734418537e869d64b53d3f
Tom_Lin <tom@hubstar.co>
2014-02-21 13:06:26 +1300

    39-fix judge the bussiness profile email

--- app_hubstar/app/scripts/controllers/ArticleController.js
+++ app_hubstar/app/scripts/controllers/ArticleController.js
@@ -179,29 +179,34 @@ HubStar.ArticleController = Ember.Controller.extend({
     JudgeBusinessProfile: function()
     {
         var currentUser = HubStar.User.find(localStorage.loginStatus);
-        var photo_owner_email = currentUser.get("email"); //photo owner contact email address
-        var endOfEmail = "";
-//        if (photo_owner_email.search("@") !== -1)
-//        {
-        endOfEmail = photo_owner_email.split("@")[1];
-
-        var trendsAccountEmail = "trendsideas.com"; //all trends account can have the edit right;
-        if ((currentUser.get("profiles") !== null && currentUser.get("profiles") !== undefined && currentUser.get("profiles") !== "") || trendsAccountEmail === endOfEmail)
-        {
-            if (currentUser.get("profiles").get("length") > 0 || trendsAccountEmail === endOfEmail)
+        var that = this;
+        currentUser.then(function() {
+            var photo_owner_email = currentUser.get("email"); //photo owner contact email address
+            var endOfEmail = "";
+            console.log(photo_owner_email);
+            if (photo_owner_email.search("@") !== -1)
             {
-                this.set("isBusinessProfile", true);
+                endOfEmail = photo_owner_email.split("@")[1];
+
+                var trendsAccountEmail = "trendsideas.com"; //all trends account can have the edit right;
+                if ((currentUser.get("profiles") !== null && currentUser.get("profiles") !== undefined && currentUser.get("profiles") !== "") || trendsAccountEmail === endOfEmail)
+                {
+                    if (currentUser.get("profiles").get("length") > 0 || trendsAccountEmail === endOfEmail)
+                    {
+                        that.set("isBusinessProfile", true);
+                    }
+                    else
+                    {
+                        that.set("isBusinessProfile", false);
+                    }
+                }
             }
             else
             {
-                this.set("isBusinessProfile", false);
+                that.set("isBusinessProfile", false);
             }
-        }
-        //  }
-//        else
-//        {
-//            this.set("isBusinessProfile", false);
-//        }
+        });
+
     },
     JudgePhotoOwner: function(mega)
     {

edc5e5cf5ac0f990854629fbb575d8b060dce0d3
sean_xu <sean@hubstar.co>
2014-03-19 11:17:38 +1300

    #841 drag and drop issue in ie solution testing

--- app_hubstar/app/scripts/views/KeywordsDropTargetView.js
+++ app_hubstar/app/scripts/views/KeywordsDropTargetView.js
@@ -1,21 +1,10 @@
 HubStar.KeywordDropTargetView = Ember.View.extend(HubStar.KeywordController.Droppable, {
     
-//        contentBinding: "Keywords",
-
-//        drop: function(event) {
-//            var controller = this.get('controller');
-//            var dataTransfer = event.originalEvent.dataTransfer;
-//            var files = dataTransfer.files;
-//            var totalFileSize = 0;
-//            (function(file) {
-//                var name = file.name;
-//               var size = file.size;
-//                var reader = new FileReader();
-//                reader.onload = function(e) {
-//                    
-//                    controller.profileStyleImageDrop(e, name,size);
-//                }, reader.readAsDataURL(files[0]);
-//            })(files[0]);
-//            return false;
-//        }
-    });
+    drop: function(event) {
+    //        var viewId = event.originalEvent.dataTransfer.getData('Text');
+    //        Ember.View.views[viewId].destroy();
+            this.get('controller').dragIntoFront();
+            event.preventDefault();
+            return false;
+    }
+});

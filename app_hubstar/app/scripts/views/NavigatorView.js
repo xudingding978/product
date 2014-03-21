34371e0fe7252a3d37082a3d7afb77a7016a6bf4
Shuai_Yang <shuai@hubstar.co>
2014-03-21 14:19:52 +1300

    save for UI fixed

--- app_hubstar/app/scripts/views/NavigatorView.js
+++ app_hubstar/app/scripts/views/NavigatorView.js
@@ -4,9 +4,9 @@ HubStar.NavigatorView = Ember.View.extend({
         var that = this;
         $(document).ready(function() {
 
-            $("#switchbar").mouseup(function(event) {
+            $("#switchbar1").mouseup(function(event) {
                 var mousedownX = event.pageX;
-                var witdhleft = $('#switchbar').offset().left;
+                var witdhleft = $('#switchbar1').offset().left;
                 var d = 23;
                 var middle = witdhleft + d;
                 if (mousedownX < middle - d / 2) {
@@ -22,11 +22,11 @@ HubStar.NavigatorView = Ember.View.extend({
 //                 $(".navbar").css("background", " url(../../images/contactbg.png)");
 //                   $(".gradient1").css("background", " linear-gradient(to bottom, #01b6e3 22%,#f5f5f5 99%)");
                     $('#discovery_search_bar_wrapper').fadeOut(500, function() {
-                        $(this).css({"background": " url(../../images/discoverybarbg.jpg)"}).fadeIn(500);
+                        $(this).css({"background": " url(../../images/discoverybarbg.jpg) no-repeat center center fixed"}).fadeIn(500);
                     });
 
                     $('.navbar').fadeOut(500, function() {
-                        $(this).css({"background": " url(../../images/landingpagebg.jpg)"}).fadeIn(500);
+                        $(this).css({"background": " url(../../images/landingpagebg.jpg) no-repeat center center fixed"}).fadeIn(500);
                     });
                     $(".gradient1").fadeOut(500, function() {
                         $(this).css("background", " linear-gradient(to bottom, #68789c 22%,#f5f5f5 99%)").fadeIn(500);
@@ -42,13 +42,13 @@ HubStar.NavigatorView = Ember.View.extend({
                     $("#Residential").css("opacity", "1");
                     localStorage.resOrcom = "all";
                     $('#discovery_search_bar_wrapper').fadeOut(500, function() {
-                        $(this).css({"background": " url(../../images/discoverybarbg.jpg)"}).fadeIn(500);
+                        $(this).css({"background": " url(../../images/discoverybarbg.jpg) no-repeat center center fixed"}).fadeIn(500);
                     });
                     $('.navbar').fadeOut(500, function() {
-                        $(this).css({"background": " url(../../images/landingpagebg.jpg)"}).fadeIn(500);
+                        $(this).css({"background": " url(../../images/landingpagebg.jpg) no-repeat center center fixed"}).fadeIn(500);
                     });
                     $(".gradient1").fadeOut(500, function() {
-                        $(this).css("background", " linear-gradient(to bottom, #68789c 22%,#f5f5f5 99%)").fadeIn(500);
+                        $(this).css("background", " linear-gradient(to bottom, #68789c 22%,#f5f5f5 99%) ").fadeIn(500);
                     });
                 }
                 else if (mousedownX >= (middle + d / 2)) {
@@ -62,11 +62,11 @@ HubStar.NavigatorView = Ember.View.extend({
                     localStorage.resOrcom = "commercial";
 //                    $('#discovery_search_bar_wrapper').css({"background": " url(../../images/commercialbg.jpg)"});
                     $('#discovery_search_bar_wrapper').fadeOut(500, function() {
-                        $(this).css({"background": " url(../../images/commercialbg.jpg)"}).fadeIn(500);
+                        $(this).css({"background": " url(../../images/commercialbg.jpg) no-repeat center center fixed"}).fadeIn(500);
                     });
 
                     $('.navbar').fadeOut(500, function() {
-                        $(this).css({"background": "url(../../images/commercialbg.jpg)"}).fadeIn(500);
+                        $(this).css({"background": "url(../../images/commercialbg.jpg) no-repeat center center fixed123"}).fadeIn(500);
                     });
                     $(".gradient1").fadeOut(500, function() {
                         $(this).css("background", " linear-gradient(to bottom, #191200 22%,#f5f5f5 99%)").fadeIn(500);
--- app_hubstar/app/scripts/views/StatusView.js
+++ app_hubstar/app/scripts/views/StatusView.js
@@ -33,65 +33,6 @@ HubStar.StatusView = Ember.View.extend({
             $('#welcome_message').attr("style", "display:block");
         }
 
-        var that = this;
-        $(document).ready(function() {
-            $("#switchbar").mouseup(function(event) {
-                var address = document.URL;
-                var type = address.split("#")[1].split("/")[2];
-
-                var mousedownX = event.pageX;
-                var witdhleft = $('#switchbar').offset().left;
-                var d = 23;
-                var middle = witdhleft + d;
-
-
-                if (mousedownX < middle - d / 2) {
-                    $('#switchbarBtn').attr("style", "margin-left:0px;");
-                    that.get("controller").get("controllers.application").set("classification", "residential");
-                    $("#Commercial").css("opacity", "0.4");
-                    $("#Residential").css("opacity", "1");
-                } else if (mousedownX >= (middle - d / 2) && mousedownX < (middle + d / 2)) {
-                    $('#switchbarBtn').attr("style", "margin-left:13px;");
-                    that.get("controller").get("controllers.application").set("classification", "All");
-                    $("#Residential").css("opacity", "1");
-                    $("#Commercial").css("opacity", "1");
-                }
-                else if (mousedownX >= (middle + d / 2)) {
-                    $('#switchbarBtn').attr("style", "margin-left:28px;");
-                    that.get("controller").get("controllers.application").set("classification", "commercial");
-                    $("#Residential").css("opacity", "0.4");
-                    $("#Commercial").css("opacity", "1");
-                }
-
-
-
-            });
-
-
-
-        });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }

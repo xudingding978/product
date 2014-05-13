733d2d9e9756c51b39ea32f33096b94ad5edec8f
sean_xu <sean@hubstar.co>
2014-05-14 10:26:54 +1200

    updated

--- app_hubstar/app/scripts/controllers/MegaController.js
+++ app_hubstar/app/scripts/controllers/MegaController.js
@@ -734,8 +734,6 @@ HubStar.MegaController = Ember.ArrayController.extend({
         var address = document.URL;
         if (this.get('controllers.masonryCollectionItems').get("type") === "profile")
         {
-
-            if (this.get("controllers.checkingLoginStatus").popupLogin()) {
                 if (this.get("from") !== "profile") //from : profile means  close from the profile collection's photo
                 {
                     // this.transitionTo("indexIndex"); //search page
@@ -779,16 +777,12 @@ HubStar.MegaController = Ember.ArrayController.extend({
                     this.transitionTo("profile", profile); // transition to profile
                     this.transitionTo("profileCollection", data);
                 }
-            }
-            else {
-                this.transitionTo("searchIndexTom");
-            }
+           
 
         }
         else if (this.get('controllers.masonryCollectionItems').get("type") === "user")
         {
 
-            if (this.get("controllers.checkingLoginStatus").popupLogin()) {
                 var collection_id = address.split("#")[1].split("/")[4];
                 var id = address.split("#")[1].split("/")[2]; //user id
                 var user = HubStar.User.find(id);
@@ -803,17 +797,11 @@ HubStar.MegaController = Ember.ArrayController.extend({
                 }
                 this.set("selectPhoto", false);
                 this.transitionTo("collection", data); //user
-            }
-            else {
-                this.transitionTo("searchIndexTom");
-            }
+            
+            
         } else {
-            this.transitionTo("searchIndexTom");
+             this.transitionTo("searchIndexTom");
         }
-     //   $("#search-bar").css('display', "none");
-   //     $("#topResidentialCommerical").css('display', "none");
-   //     $(".search-bar-on-small-screen").css('display', "none");
-
     },
     editingContactForm: function() {
 

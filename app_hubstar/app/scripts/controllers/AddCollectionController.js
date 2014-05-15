1832830515c0dcc9d4195301dece7b46ae5fcfc7
sean_xu <sean@hubstar.co>
2014-05-16 10:51:58 +1200

    #1034 fixed the scroll issue

--- app_hubstar/app/scripts/controllers/AddCollectionController.js
+++ app_hubstar/app/scripts/controllers/AddCollectionController.js
@@ -142,7 +142,6 @@ HubStar.AddCollectionController = Ember.ObjectController.extend({
                         var that = this;
                         this.set("commentObject", HubStar.Mega.find(this.get("objectID")));
                         requiredBackEnd('collections', 'saveCollection', data, 'POST', function(params) {
-                            //console.log(params);
                             HubStar.get('selectedCollection').collection_ids = params;
                             var tempComment = [that.get("objectID")];
                             //that.commitCollection();
@@ -159,9 +158,12 @@ HubStar.AddCollectionController = Ember.ObjectController.extend({
                     }
                 }
             }
+            $("#body_id").css("overflow", "auto");
         } else {
             this.get('controllers.applicationFeedback').statusObserver(null, "Please choose a collection.", "warnning");
         }
+        
+        
 
     },
     commitCollection: function() {
@@ -200,9 +202,6 @@ HubStar.AddCollectionController = Ember.ObjectController.extend({
             var name = currentUser.get('display_name');
             var date = new Date();
             var message_id = createMessageid() + commenter_id;
-//             var tempComment = HubStar.Comment.createRecord({"commenter_profile_pic_url": commenter_profile_pic_url, 
-//                 "message_id": message_id, "commenter_id": commenter_id, "name": name, "content": commentContent, "time_stamp": date.toString(), 
-//                 "is_delete": false, optional: this.get('mega').get('type') + '/' + this.get('mega').get('id')});
 
             var tempComment = HubStar.Comment.createRecord({"commenter_profile_pic_url": commenter_profile_pic_url,
                 "message_id": message_id, "commenter_id": commenter_id, "name": name, "content": commentContent, "time_stamp": date.toString(),
@@ -295,7 +294,6 @@ HubStar.AddCollectionController = Ember.ObjectController.extend({
     exit: function() {
         this.set('selectionPop', false);
         this.set('selectionProfile', false);
-        //console.log(this.get("parentTController"));
         if (this.get('parentTController') === 'article')
         {
             this.get("controllers.article").switchCollection();
@@ -315,6 +313,7 @@ HubStar.AddCollectionController = Ember.ObjectController.extend({
         else {
             this.get("controllers.mega").switchCollection();
         }
+        $("#body_id").css("overflow", "auto");
     },
     addNewCollection: function()
     {

08c809534c5e5d43bf121130f2774613f8a10f70
sean_xu <sean@hubstar.co>
2014-05-19 10:56:41 +1200

    fixed the profile pic issue but welcome page need shuai help

--- app_hubstar/app/scripts/routes/SearchIndexTomRoute.js
+++ app_hubstar/app/scripts/routes/SearchIndexTomRoute.js
@@ -1,12 +1,12 @@
 HubStar.SearchIndexTom = Ember.Route.extend({
-    setupController: function() {     
+    setupController: function() {
         if (localStorage.getItem("loginStatus") === null || (localStorage.loginStatus === "")) {
             HubStar.set('isLogin', false);
 
         } else {
             HubStar.set('isLogin', true);
         }
-        HubStar.set("escVideo",false);
+        HubStar.set("escVideo", false);
         this.controllerFor('article').set("accessFromSearchBoard", true);
         this.controllerFor('searchs').defaultSearch();
         this.controllerFor('index').setLogin();
@@ -14,19 +14,26 @@ HubStar.SearchIndexTom = Ember.Route.extend({
         this.controllerFor('status').set('islogin', true);
         this.controllerFor('application').set('popup', false);
         this.controllerFor('application').set('isotherpage', false);
-        localStorage.checkUser = "";
+//        if (localStorage.checkUser === "newUser") {
+//            console.log("ssssssssss");
+//            this.controllerFor('application').set('isNewUser', true);
+//        } else {
+//            localStorage.checkUser = "";
+//            this.controllerFor('application').set('isNewUser', false);
+//        }
+         localStorage.checkUser = "";
         $(".navbar").css("box-shadow", "");
         var that = this;
-         $(document).ready(function() {
-             setTimeout(function() {
-        that.controllerFor('application').searchSmallScreen();
-             },50);
-         });
-       // $(".navbar").css("background", " url(../../images/landingpagebg.jpg)");
-       // $('#masonry_wrapper').attr('style', "top:100px;position:relative");
-        
+        $(document).ready(function() {
+            setTimeout(function() {
+                that.controllerFor('application').searchSmallScreen();
+            }, 50);
+        });
+        // $(".navbar").css("background", " url(../../images/landingpagebg.jpg)");
+        // $('#masonry_wrapper').attr('style', "top:100px;position:relative");
+
     },
-    events: { 
+    events: {
     },
     redirect: function() {
 
--- app_hubstar/app/scripts/routes/WelcomeRoute.js
+++ app_hubstar/app/scripts/routes/WelcomeRoute.js
@@ -1,36 +1,36 @@
 HubStar.WelcomeRoute = Ember.Route.extend({
-    redirect: function() {
-        if (localStorage.checkUser === "newUser") {
-
-
-        } else {
-
-            this.transitionTo('searchIndexTom');
-
-        }
-
-
-    },
-    activate: function() {
-        HubStar.set("isLogin", true);
-    },
-    setupController: function() {
-        this.controllerFor('searchs').defaultSearch();
-        this.controllerFor('index').setLogin();
-
-        this.controllerFor('application').set('islogin', true);
-
-        this.controllerFor('status').set('islogin', true);
-        this.controllerFor('application').set('popup', false);
-        this.controllerFor('application').set('isotherpage', true);
-
-    },
-    renderTemplate: function() {
-
-        this.render('welcome', {
-            outlet: 'welcome',
-            into: 'application'
-
-        });
-    }
+//    redirect: function() {
+//        if (localStorage.checkUser === "newUser") {
+//
+//
+//        } else {
+//
+//            this.transitionTo('searchIndexTom');
+//
+//        }
+//
+//
+//    },
+//    activate: function() {
+//        HubStar.set("isLogin", true);
+//    },
+//    setupController: function() {
+//        this.controllerFor('searchs').defaultSearch();
+//        this.controllerFor('index').setLogin();
+//
+//        this.controllerFor('application').set('islogin', true);
+//
+//        this.controllerFor('status').set('islogin', true);
+//        this.controllerFor('application').set('popup', false);
+//      // this.controllerFor('application').set('isotherpage', true);
+//
+//    },
+//    renderTemplate: function() {
+//
+//        this.render('welcome', {
+//            outlet: 'welcome',
+//            into: 'application'
+//
+//        });
+//    }
 });

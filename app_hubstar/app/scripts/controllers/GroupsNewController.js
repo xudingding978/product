172a3dc467b663c326fe70f116cd65d8bbed65c2
dingding_xu <dingding@hubstar.co>
2014-05-20 14:10:31 +1200

    save work

--- app_hubstar/app/scripts/controllers/GroupsNewController.js
+++ app_hubstar/app/scripts/controllers/GroupsNewController.js
@@ -5,26 +5,38 @@ HubStar.GroupsNewController = Ember.Controller.extend({
     groupStepFour: false,
     categorys: [],
     subcate: [],
+    selected_topics: "",
     isResidential: true,
     needs: ['profile', 'applicationFeedback', 'application'],
     init: function()
     {
-        this.setTopicModel(HubStar.Cate.find({}));
+        this.setTopicModel();
     },
-    setTopicModel: function(model) {
-        this.set('categorys', null);
-        this.set('categorys', model);
-
+    setTopicModel: function() {
+        this.set('categorys', this.get("controllers.application").get("categorys"));
     },
     topicSelection: function(data) {
-
         this.set('subcate', []);
         for (var i = 0; i < data.get('subcate').get('length'); i++)
         {
             this.get('subcate').pushObject({'category_topic': data.get('subcate').objectAt(i).get('category_topic'), 'subcategories': data.get('subcate').objectAt(i).get('subcategories')
             });
         }
-
+    },
+    selectTopic: function(ids, topic) {
+        if (this.get('selected_topics').indexOf(topic) !== -1) {
+            this.set('selected_topics', this.get('selected_topics').replace(topic, ""));
+            this.set('selected_topics', this.get('selected_topics').replace(",,", ","));
+            $('#minusSelect_' + ids).attr("style", "opacity: .8; z-index: 10; right: 0; margin: 10px; display:none;");
+        }
+        else {
+            $('#minusSelect_' + ids).attr("style", "opacity: .8; z-index: 10; right: 0; display:block;");
+            if (this.get('selected_topics') === "") {
+                this.set('selected_topics', topic);
+            } else {
+                this.set('selected_topics', this.get('selected_topics') + "," + topic);
+            }
+        }
     },
     groupStep: function(number) {
 
@@ -45,6 +57,7 @@ HubStar.GroupsNewController = Ember.Controller.extend({
             this.set("groupStepFour", true);
         } else if (number === "0") {
             this.set("groupStepOne", true);
+            this.set("selected_topics", '');
             this.set("groupStepTwo", false);
             this.set("groupStepThree", false);
             this.set("groupStepFour", false);
@@ -65,33 +78,35 @@ HubStar.GroupsNewController = Ember.Controller.extend({
         //this.fillInChecking();
         //if (passSubmit) 
         {
-            var newMegaNewModel = HubStar.Meganew.createRecord({
-                "id": createMessageid(),
+            var that = this;
+            var id =createMessageid();
+            var newMegaModel = HubStar.Mega.createRecord({
+                "id": id + "",
                 "type": "group",
                 boost: 25,
                 accessed: new Date(),
                 is_active: true,
                 is_indexed: true,
                 is_deleted: false,
-                categories: $('#categorySelection').text(),
-                subcategories: $('#subcategorySelection').text(),
+                categories: that.get("selected_topics"),
+                subcategories: "",
                 created: new Date(),
                 creator: localStorage.loginStatus,
-                classification: this.get("classification"),
-                country: $('#countrySelection').text(),
-                region: $('#regionSelection').text(),
-                suburb: this.get("suburb"),
+                classification: that.get("isResidential") ? "residential" : "commercial",
+                country: null,
+                region: null,
+                suburb: null,
                 domains: getDomain(),
-                editors: this.get("owner") + "," + this.get("editors"),
-                keywords: this.get("keywords"),
-                keyword_num: this.get("keywordNumber"),
-                owner_type: "profiles", // profiles or user can upload files, this could help to link back to their profile.
+                editors: "",
+                keywords: "",
+                keyword_num: 0,
+                owner_type: "groups", // profiles or user can upload files, this could help to link back to their profile.
                 owner_profile_pic: "https://s3-ap-southeast-2.amazonaws.com/develop.devbox/profile_pic/default/defaultpic1.jpg",
-                owner_title: this.get("profile_name"), //profile name
-                owner_id: this.get("profile_url"), //profile id
-                owner_contact_email: this.get("direct_enquiry_emails"),
-                owner_contact_bcc_emails: this.get("direct_enquiry_emails_2"),
-                owner_contact_cc_emails: this.get("direct_enquiry_emails_3"),
+                owner_title: "", //profile name
+                owner_id: localStorage.loginStatus, //user id
+                owner_contact_email: null,
+                owner_contact_bcc_emails: null,
+                owner_contact_cc_emails: null,
                 updated: 0,
                 view_count: 0,
                 share_count: 0,
@@ -99,71 +114,44 @@ HubStar.GroupsNewController = Ember.Controller.extend({
                 likes_count: 0,
                 comment_count: 0,
                 keyword: [],
-                profile: []
+                group: []
             });
 
-            var newProfile = HubStar.Profile.createRecord({
-                id: this.get("profile_url"),
-                profile_name: this.get("profile_name"),
-                profile_contact_last_name: this.get("last_name"),
-                profile_contact_first_name: this.get("first_name"),
-                profile_about_us: "<br>Welcome!<br>",
-                profile_package_name: this.get("profile_package"),
-                profile_bg_url: "https://s3-ap-southeast-2.amazonaws.com/develop.devbox/profile_bg/default/defaultbg6.jpg",
-                profile_hero_url: "https://s3-ap-southeast-2.amazonaws.com/develop.devbox/profile_cover/default/defaultcover4.jpg",
-                profile_pic_url: "https://s3-ap-southeast-2.amazonaws.com/develop.devbox/profile_pic/default/defaultpic1.jpg",
-                profile_boost: this.get("keywordNumber"),
-                owner: this.get("owner"),
-                profile_creator: localStorage.loginStatus,
-                profile_administrator: "",
-                profile_editor: "",
-                profile_editors: this.get("owner") + "," + this.get("editors"),
-                profile_contact_number: this.get("profile_contact_number"),
-                owner_contact_email: this.get("direct_enquiry_emails"),
-                owner_contact_bcc_emails: this.get("direct_enquiry_emails_2"),
-                owner_contact_cc_emails: this.get("direct_enquiry_emails_3"),
-                profile_category: $('#categorySelection').text(),
-                profile_subcategory: $('#subcategorySelection').text(),
-                profile_physical_address: this.get("address"),
-                profile_is_active: "true",
-                profile_is_deleted: "false",
-                profile_suburb: this.get("suburb"),
-                profile_keywords: this.get("keywords"),
-                profile_video_num: 0,
-                profile_keywords_num: this.get("keywordNumber"),
-                profile_regoin: $('#regionSelection').text(),
-                profile_country: $('#countrySelection').text(),
-                profile_hours: "Monday=9:00am-5:00pm,Tuesday=9:00am-5:00pm,Wednesday=9:00am-5:00pm,Thursday=9:00am-5:00pm,Friday=9:00am-5:00pm,Saturday=closed,Sunday=closed,Holidays=closed",
-                profile_partner_ids: null,
-                profile_website_url: this.get("website_url"),
-                profile_website: this.get("website"),
+            var newGroup = HubStar.Group.createRecord({
+                group_step: "",
+                group_budget: "",
+                group_expertise: "",
+                group_category: that.get("selected_topics"),
+                group_subcategory: "",
+                group_hero_url: "https://s3-ap-southeast-2.amazonaws.com/develop.devbox/profile_bg/default/defaultbg6.jpg",
+                group_pic_url: "https://s3-ap-southeast-2.amazonaws.com/develop.devbox/profile_cover/default/defaultcover4.jpg",
+                group_bg_url: "https://s3-ap-southeast-2.amazonaws.com/develop.devbox/profile_pic/default/defaultpic1.jpg",
+                group_hero_cover_url: "",
+                group_name: "",               
+                group_time: "",//how long does the project need?
+                group_partner_ids: "",
+                group_creator: localStorage.loginStatus, //user id
+                group_administrator: "", //user id
+                group_editor: "", //user id
+                id: id,
                 keywords: []
-
             });
 
-            var that = this;
-            requiredBackEnd('meganews', 'createNewProfile', newMegaNewModel, 'POST', function(params) {
-                if (params === true) {
-                    that.get('controllers.applicationFeedback').statusObserver(null, "The profile has been created with the same profile URL, Please change a new profile name, thank you!", "warnning");
-                }
-                else {
-                    newMegaNewModel.save();
-                    newMegaNewModel.get('isSaving');
-                    newMegaNewModel.addObserver('isDirty', function() {
-                        if (!newMegaNewModel.get('isDirty')) {
-                            newProfile.save();
-                            newProfile.get('isSaving');
-                            newProfile.addObserver('isDirty', function() {
-                                if (!newProfile.get('isDirty')) {
-                                    location.href = "#/profiles/" + that.get("profile_url");
-                                    location.reload();
-                                }
-                            });
-                        }
-                        else
-                        {
+            newMegaModel.save();
+            newMegaModel.get('isSaving');
+            newMegaModel.addObserver('isDirty', function() {
+                if (!newMegaModel.get('isDirty')) {
+                    newGroup.save();
+                    newGroup.get('isSaving');
+                    newGroup.addObserver('isDirty', function() {
+                        if (!newGroup.get('isDirty')) {
+//                            location.href = "#/profiles/" + that.get("profile_url");
+//                            location.reload();
                         }
                     });
+                }
+                else
+                {
                 }
             });
         }
---
+++ app_restAPI/protected/controllers/GroupsController.php
@@ -0,0 +1,345 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header('Content-type: *');
+
+header('Access-Control-Request-Method: *');
+header('Access-Control-Allow-Methods: PUT, POST, OPTIONS,GET');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+Yii::import('ext.runactions.components.ERunActions');
+
+class GroupsController extends Controller {
+
+    const JSON_RESPONSE_ROOT_SINGLE = 'profile';
+    const JSON_RESPONSE_ROOT_PLURAL = 'profiles';
+
+    public function actionIndex() {
+    }
+
+    public function actionCreate() {
+        try {
+            $request_json = file_get_contents('php://input');
+            $request_arr = CJSON::decode($request_json, true);
+            error_log(var_export($request_arr, true));
+            $tempProfile = $request_arr['group'];
+            $cb = $this->couchBaseConnection();
+            $id = $tempProfile['id'];
+            $domain = $this->getDomain();
+            $docID = $domain . "/groups/" . $id;
+            $tempMega = $cb->get($docID);
+            $mega = CJSON::decode($tempMega, true);
+            $mega['groups'][0] = $tempProfile;
+            $mega['groups'][0]['collections'] = array();
+
+
+            if ($cb->set($docID, CJSON::encode($mega))) {
+                $this->sendResponse(204);
+            } else {
+                echo $this->sendResponse(409, 'A record with id: "' . substr($_SERVER['HTTP_HOST'], 4) . $_SERVER['REQUEST_URI'] . '/' . '" already exists');
+            }
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
+            echo json_decode(file_get_contents('php://input'));
+        }
+    }
+
+    public function actionRead() {
+        try {
+            $cb = $this->couchBaseConnection();
+            $fileName = $this->getDomain() . $_SERVER['REQUEST_URI'];
+            $reponse = $cb->get($fileName);
+            $request_arr = CJSON::decode($reponse, true);
+            $respone_client_data = str_replace("\/", "/", CJSON::encode($request_arr["profile"][0]));
+            $result = '{"' . self::JSON_RESPONSE_ROOT_SINGLE . '":';
+//Iterate over the hits and print out some data
+            if($request_arr!==null){
+            $result .=$respone_client_data;
+            }
+            $result .= '}';
+
+            echo $this->sendResponse(200, $result);
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
+        }
+    }
+
+    public function actionUpdate() {
+
+        try {
+            $payloads_arr = CJSON::decode(file_get_contents('php://input'));
+            $payload_json = CJSON::encode($payloads_arr['profile'], true);
+            $newRecord = CJSON::decode($payload_json);
+            $cb = $this->couchBaseConnection();
+            $oldRecord = CJSON::decode($cb->get($this->getDomain() . $_SERVER['REQUEST_URI']));
+            $oldRecord['profile'][0]['owner'] = $newRecord['owner'];
+            $oldRecord['profile'][0]['owner_contact_bcc_emails'] = $newRecord['owner_contact_bcc_emails'];
+            $oldRecord['profile'][0]['owner_contact_cc_emails'] = $newRecord['owner_contact_cc_emails'];
+            $oldRecord['profile'][0]['owner_contact_email'] = $newRecord['owner_contact_email'];
+            $oldRecord['profile'][0]['profile_about_us'] = $newRecord['profile_about_us'];
+            $oldRecord['profile'][0]['profile_areas_serviced'] = $newRecord['profile_areas_serviced'];
+            $oldRecord['profile'][0]['profile_boost'] = $newRecord['profile_boost'];
+            $oldRecord['profile'][0]['profile_contact_first_name'] = $newRecord['profile_contact_first_name'];
+            $oldRecord['profile'][0]['profile_contact_last_name'] = $newRecord['profile_contact_last_name'];
+            $oldRecord['profile'][0]['profile_contact_number'] = $newRecord['profile_contact_number'];
+            if ($oldRecord['profile'][0]['profile_name'] !== $newRecord['profile_name']) {
+                $oldRecord['profile'][0]['profile_name'] = $newRecord['profile_name'];
+                $setProfileName = TRUE;
+            } else {
+                $setProfileName = FALSE;
+            }
+            $oldRecord['profile'][0]['profile_category'] = $newRecord['profile_category'];
+            $oldRecord['profile'][0]['profile_subcategory'] = $newRecord['profile_subcategory'];
+            $oldRecord['profile'][0]['profile_country'] = $newRecord['profile_country'];
+            $oldRecord['profile'][0]['profile_domains'] = $newRecord['profile_domains'];
+            $oldRecord['profile'][0]['profile_country'] = $newRecord['profile_country'];
+            $oldRecord['profile'][0]['profile_editors'] = $newRecord['profile_editors'];
+            
+            $oldRecord['profile'][0]['profile_editor'] = $newRecord['profile_editor'];
+            $oldRecord['profile'][0]['profile_administrator'] = $newRecord['profile_administrator'];
+            $oldRecord['profile'][0]['profile_creator'] = $newRecord['profile_creator'];
+            
+            $oldRecord['creator'] = $newRecord['profile_creator'];
+            $oldRecord['editors'] = $newRecord['profile_editors'];
+            $oldRecord['profile'][0]['profile_hours'] = $newRecord['profile_hours'];
+            $oldRecord['profile'][0]['profile_is_active'] = $newRecord['profile_is_active'];
+            $oldRecord['profile'][0]['profile_is_deleted'] = $newRecord['profile_is_deleted'];
+            $oldRecord['profile'][0]['profile_keywords'] = $newRecord['profile_keywords'];
+            $oldRecord['profile'][0]['profile_video_num'] = $newRecord['profile_video_num'];
+            $oldRecord['keywords'] = $newRecord['profile_keywords'];
+            $oldRecord['profile'][0]['profile_keywords_num'] = $newRecord['profile_keywords_num'];
+            $oldRecord['keyword_num'] = $newRecord['profile_keywords_num'];
+            $oldRecord['profile'][0]['title_modify_time'] = $newRecord['title_modify_time'];
+
+
+//            $oldRecord['profile'][0]['keywords'] = $newRecord['keywords'] ;
+//            $oldRecord['keyword'] = $newRecord['keywords'];
+            if ($oldRecord['profile'][0]['profile_package_name'] !== $newRecord['profile_package_name']) {
+                $oldRecord['profile'][0]['profile_package_name'] = $newRecord['profile_package_name'];
+                $boost = $this->setBoost($newRecord['profile_package_name']);
+                $oldRecord['profile'][0]['profile_boost'] = $boost;
+                $oldRecord['boost']=$boost;
+                $setPhotoBoost = TRUE;
+            } else {
+                $setPhotoBoost = FALSE;
+            }
+            $oldRecord['profile'][0]['profile_partner_ids'] = $newRecord['profile_partner_ids'];
+            $oldRecord['profile'][0]['profile_physical_address'] = $newRecord['profile_physical_address'];
+            $oldRecord['profile'][0]['profile_suburb'] = $newRecord['profile_suburb'];
+            $oldRecord['profile'][0]['profile_regoin'] = $newRecord['profile_regoin'];
+            $oldRecord['profile'][0]['profile_website'] = $newRecord['profile_website'];
+            $oldRecord['profile'][0]['profile_website_url'] = $newRecord['profile_website_url'];
+            $oldRecord['profile'][0]['profile_cover_text'] = $newRecord['profile_cover_text'];
+            $oldRecord['profile'][0]['profile_facebook_link'] = $newRecord['profile_facebook_link'];
+            $oldRecord['profile'][0]['profile_twitter_link'] = $newRecord['profile_twitter_link'];
+            $oldRecord['profile'][0]['profile_googleplus_link'] = $newRecord['profile_googleplus_link'];
+            $oldRecord['profile'][0]['profile_pinterest_link'] = $newRecord['profile_pinterest_link'];
+            $oldRecord['profile'][0]['profile_linkedin_link'] = $newRecord['profile_linkedin_link'];
+            $oldRecord['profile'][0]['profile_youtube_link'] = $newRecord['profile_youtube_link'];
+            $oldRecord['profile'][0]['profile_analytics_code'] = $newRecord['profile_analytics_code'];
+            $oldRecord['profile'][0]['profile_google_map'] = $newRecord['profile_google_map'];
+
+            $oldRecord['profile'][0]['show_template'] = $newRecord['show_template'];
+
+            $oldRecord['profile'][0]['show_keyword_id'] = $newRecord['show_keyword_id'];
+            $cb->set($this->getDomain() . $_SERVER['REQUEST_URI'], CJSON::encode($oldRecord, true));
+//            if ($setProfileName) {
+////                ERunActions::touchUrlExt('http://api.develop.trendsideas.com/profiles/backgroundProcess',$postData=null,$contentType=null,$httpClientConfig=array());
+//                ERunActions::httpPOST('http://api.develop.trendsideas.com/profiles/backgroundProcess',array('profile_id'=>$oldRecord['profile'][0]['id'],'profile_name'=>$newRecord['profile_name']));
+//            }
+            if ($cb->set($this->getDomain() . $_SERVER['REQUEST_URI'], CJSON::encode($oldRecord, true))) {
+                $this->sendResponse(204);
+            }
+//            if ($setProfileName) {
+//                $this->setProfileName($newRecord['profile_name'], $oldRecord['profile'][0]['id']);
+//            }
+//            if ($setPhotoBoost) {
+//                $this->setPhotoBoost($oldRecord['profile'][0]['profile_boost'], $oldRecord['profile'][0]['id']);
+//            }
+        } catch (Exception $exc) {
+        }
+    }
+
+
+    public function modifyOwnerID($data_arr, $profile_name, $log_path) {
+        $cb = $this->couchBaseConnection();
+
+        for ($i = 0; $i < sizeof($data_arr); $i++) {
+            try {
+                $docID = $data_arr[$i];
+//            $this->writeToLog('/var/log/nginx/backprocess.log', $docID);
+                $profileOwn = $cb->get($docID);
+
+//            $this->writeToLog('/var/log/nginx/backprocess.log', $docID . ' get');
+                $owner = CJSON::decode($profileOwn, true);
+//            $this->writeToLog('/var/log/nginx/backprocess.log', $docID. 'decode');
+
+                $owner['owner_title'] = $profile_name;
+                if ($cb->set($docID, CJSON::encode($owner))) {
+                    array_splice($data_arr, $i, 1);
+//                    $this->writeToLog($log_path, $docID . 'update success');
+                } else {
+//                    $this->writeToLog($log_path, $docID . 'update fail'.'since');
+                }
+            } catch (Exception $e) {
+//                $this->writeToLog($log_path, 'error when get data');
+            }
+        }
+        return $data_arr;
+    }
+
+    public function findAllAccordingOwner($owner_id) {
+        $request = $this->getElasticSearch();
+        $must = Sherlock\Sherlock::queryBuilder()->QueryString()->query("\"$owner_id\"")
+                ->default_field('couchbaseDocument.doc.owner_id');
+        $bool = Sherlock\Sherlock::queryBuilder()->Bool()->must($must);
+        $data_arr = array();
+        for ($i = 0; $i < 2000; $i++) {
+            $request->from($i * 50)
+                    ->size(50);
+            $request->query($bool);
+            $response = $request->execute();
+            foreach ($response as $hit) {
+                //     echo $hit["score"] . ' - ' . $hit['id'] . "\r\n";
+                array_push($data_arr, $hit['id']);
+//                 $this->writeToLog('/var/log/nginx/backprocess.log', $hit['id']);
+            }
+            if (sizeof($response) == 0) {
+                $i = 2000;
+            }
+        }
+        return $data_arr;
+    }
+
+    public function setBoost($package_name) {
+        $domain = $this->getDomain();
+        $configuration = $this->getProviderConfigurationByName($domain, "package_details");
+        $boost = $configuration[$package_name]['boost'];
+        return $boost;
+    }
+
+    public function actionSetPhotoBoost($boost, $profile_id) {
+        $response = $this->getProfileReults($profile_id);
+        $responseArray = array();
+        foreach ($response as $hit) {
+            $id = $hit['source']['doc']['id'];
+            $profileId = $hit['source']['doc']['owner_id'];
+            if ($profileId === $profile_id) {
+                $cb = $this->couchBaseConnection();
+                $docID = $this->getDomain() . '/' . $id;
+                $profileOwn = $cb->get($docID);
+                $owner = CJSON::decode($profileOwn, true);
+                $owner['boost'] = $boost;
+
+                if ($cb->set($docID, CJSON::encode($owner))) {
+                    array_unshift($responseArray, $id . ' update succeed');
+                } else {
+                    array_unshift($responseArray, $id . ' delete failed');
+                }
+            }
+        }
+    }
+
+    public function actionSetProfileName() {
+        $payloads_arr = CJSON::decode(file_get_contents('php://input'));
+        $infoDel = CJSON::decode($payloads_arr, true);
+        $profile_name = $infoDel[0];
+        $profile_id = $infoDel[1];
+        $response = $this->getProfileReults($profile_id);
+        $responseArray = array();
+        foreach ($response as $hit) {
+            $id = $hit['source']['doc']['id'];
+            $profileId = $hit['source']['doc']['owner_id'];
+            if ($profileId === $profile_id) {
+                $cb = $this->couchBaseConnection();
+                $docID = $this->getDomain() . '/' . $id;
+                $profileOwn = $cb->get($docID);
+                $owner = CJSON::decode($profileOwn, true);
+                $owner['owner_title'] = $profile_name;
+
+                if ($cb->set($docID, CJSON::encode($owner))) {
+                    array_unshift($responseArray, $id . ' update succeed');
+                } else {
+                    array_unshift($responseArray, $id . ' delete failed');
+                }
+            }
+        }
+    }
+    
+    public function actionDelete() {
+        try {
+            
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
+        }
+    }
+
+    public function actionUpdateStyleImage() {
+
+        $payloads_arr = CJSON::decode(file_get_contents('php://input'));
+
+        $photo_string = $payloads_arr['newStyleImageSource'];
+        $photo_name = $payloads_arr['newStyleImageName'];
+
+        $mode = $payloads_arr['mode'];
+        $owner_id = $payloads_arr['id'];
+        $photoController = new PhotosController();
+        $data_arr = $photoController->convertToString64($photo_string);
+        $photo = imagecreatefromstring($data_arr['data']);
+        $compressed_photo = $photoController->compressPhotoData($data_arr['type'], $photo);
+        $orig_size['width'] = imagesx($compressed_photo);
+        $orig_size['height'] = imagesy($compressed_photo);
+
+        $url = $photoController->savePhotoInTypes($orig_size, $mode, $photo_name, $compressed_photo, $data_arr, $owner_id, $mode);
+
+        $cb = $this->couchBaseConnection();
+        $oldRecord = CJSON::decode($cb->get($this->getDomain() . '/profiles/' . $owner_id));
+
+        if ($mode == 'profile_hero') {
+            $oldRecord['profile'][0]['profile_hero_url'] = null;
+            $oldRecord['profile'][0]['profile_hero_url'] = $url;
+        } elseif
+        ($mode == 'background') {
+            $oldRecord['profile'][0]['profile_bg_url'] = null;
+            $oldRecord['profile'][0]['profile_bg_url'] = $url;
+        } elseif
+        ($mode == 'profile_picture') {
+            $oldRecord['profile'][0]['profile_pic_url'] = null;
+            $oldRecord['profile'][0]['profile_pic_url'] = $url;
+        }
+
+        if ($mode == 'profile_hero') {
+            $smallimage = $photoController->savePhotoInTypes($orig_size, 'hero', $photo_name, $compressed_photo, $data_arr, $owner_id, $mode);
+            $oldRecord['profile'][0]['profile_hero_cover_url'] = null;
+            $oldRecord['profile'][0]['profile_hero_cover_url'] = $smallimage;
+        }
+
+        $url = $this->getDomain() . '/profiles/' . $owner_id;
+
+        $copy_of_oldRecord = unserialize(serialize($oldRecord));
+        $tempUpdateResult = CJSON::encode($copy_of_oldRecord, true);
+
+        if ($cb->delete($url)) {
+            if ($cb->set($url, $tempUpdateResult)) {
+                $this->sendResponse(204, $url);
+            } else {
+                $this->sendResponse(500, 'something wrong');
+            }
+        } else {
+            $cb->set($url, $tempUpdateResult);
+            $this->sendResponse(500, 'something wrong');
+        }
+    }
+
+    public function writeToLog($fileName, $content) {
+//   $my_file = '/home/devbox/NetBeansProjects/test/addingtocouchbase_success.log';
+        $handle = fopen($fileName, 'a') or die('Cannot open file:  ' . $fileName);
+        $output = "\n" . $content;
+        fwrite($handle, $output);
+        fclose($handle);
+
+        unset($fileName, $content, $handle, $output);
+    }
+
+}
+
+?>

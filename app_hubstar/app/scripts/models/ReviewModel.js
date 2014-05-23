d65bc2f2fdb5dcb298186515117fdbe541d0207f
Shuai_Yang <shuai@hubstar.co>
2014-05-23 16:56:58 +1200

    change package.json file

--- app_hubstar/app/scripts/models/ReviewModel.js
+++ app_hubstar/app/scripts/models/ReviewModel.js
@@ -23,8 +23,21 @@ HubStar.Review = DS.Model.extend({
     review_people_like:DS.attr('string'),
     reply_reviews: DS.hasMany('HubStar.Reply'), 
     optional: DS.attr('string'),
+    isLike: false,
     getUser: function() {
         return this.get('review_user_id') === localStorage.loginStatus;
-    }.property('isUserSelf')
+    }.property('isUserSelf'),
+    review_isLike: function() {
+        if (this.get("review_people_like") !== null) {
+            if (this.get("review_people_like").indexOf(localStorage.loginStatus) !== -1) {
+                this.set("isLike", true);
+                
+            } else {
+                this.set("isLike", false);
+            }
+        } else {
+            this.set("isLike", false);
+        }
+    }.property("review_people_like")
  
 });

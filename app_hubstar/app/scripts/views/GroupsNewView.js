1b733b9ad090845fbc17f680504df7234b1bfe1b
sean_xu <sean@hubstar.co>
2014-05-26 11:03:07 +1200

    save

--- app_hubstar/app/scripts/views/GroupsNewView.js
+++ app_hubstar/app/scripts/views/GroupsNewView.js
@@ -1,7 +1,8 @@
 HubStar.GroupsNewView = Ember.View.extend({
     templateName: 'groupsNew',
     didInsertElement: function() {
-
+   console.log("slider");
+        $("#ex2").slider({});
     }
 });
 

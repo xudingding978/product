2064f19c6ff97f742e13efb5845fd81be3b2164e
Kingsley_Huang <kingsley@hubstar.co>
2014-05-28 16:39:26 +1200

    implement mail chimp

---
+++ app_restAPI/protected/components/testClass.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of testClass
+ *
+ * @author devbox
+ */
+class testClass {
+        public $api_key;
+    function __construct($api_key) {
+        $this->api_key = $api_key;
+      
+    }
+}
